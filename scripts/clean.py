3f3d26d635f1bbd5afa2664d0d060746e1d5f0ba
Jeremy Fincher <jemfinch@users.sourceforge.net>
2003-04-02 08:00:57 +0000

    Added check to make sure logs/* were normal files.

--- scripts/clean.py
+++ scripts/clean.py
@@ -11,7 +11,9 @@ def removeFiles(arg, dirname, names):
 
 if __name__ == '__main__':
     for name in os.listdir('logs'):
-        os.remove(os.path.join('logs', name))
+        filename = os.path.join('logs', name)
+        if os.path.isfile(filename):
+            os.remove(filename)
     os.path.walk(os.curdir, removeFiles, None)
 
 # vim:set shiftwidth=4 tabstop=8 expandtab textwidth=78:
