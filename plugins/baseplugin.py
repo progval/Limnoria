f39b35eb9b316ec6bb66cff270539991a6931ded
Jeremy Fincher <jemfinch@users.sourceforge.net>
2003-04-11 20:52:39 +0000

    Must've forgotten this one earlier.

--- plugins/baseplugin.py
+++ plugins/baseplugin.py
@@ -12,6 +12,7 @@ import os
 import cdb
 import conf
 import world
+import ircutils
 import callbacks
 
 class ChannelDBHandler(object):
@@ -19,7 +20,7 @@ class ChannelDBHandler(object):
     """
     suffix = '.db'
     def __init__(self, suffix='.db'):
-        self.dbCache = {}
+        self.dbCache = ircutils.IrcDict()
         suffix = self.suffix
         if self.suffix and self.suffix[0] != '.':
             suffix = '.' + suffix
@@ -34,7 +35,6 @@ class ChannelDBHandler(object):
         return cdb.shelf(filename)
 
     def getDb(self, channel):
-        channel = ircutils.toLower(channel)
         try:
             return self.dbCache[channel]
         except KeyError:
