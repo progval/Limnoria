2230149cf0f42509fcab236901d4685ec3f8f5ba
Jeremy Fincher <jemfinch@users.sourceforge.net>
2003-04-14 05:59:41 +0000

    Fixed moduleof command.

--- src/MiscCommands.py
+++ src/MiscCommands.py
@@ -184,7 +184,7 @@ class MiscCommands(callbacks.Privmsg):
         command = privmsgs.getArgs(args)
         Class = irc.findCallback(command)
         if Class is not None:
-            irc.reply(msg, Class.__name__)
+            irc.reply(msg, Class.__class__.__name__)
         else:
             irc.error(msg, 'There is no such command %s' % command)
 
