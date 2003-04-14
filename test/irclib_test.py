ab1d8cecd602cda6f34caac89abad011ac24a51d
Jeremy Fincher <jemfinch@users.sourceforge.net>
2003-04-14 07:22:06 +0000

    Fixed typo in irclib, forgot to fix it here.

--- test/irclib_test.py
+++ test/irclib_test.py
@@ -144,7 +144,7 @@ class IrcTestCase(unittest.TestCase):
         self.failIf(self.irc.fastqueue)
         self.failIf(self.irc.state.history)
         self.failIf(self.irc.state.channels)
-        self.failIf(self.irc.outstandingPings)
+        self.failIf(self.irc.outstandingPongs)
         self.assertEqual(self.irc._nickmods, conf.nickmods)
 
     def testHistory(self):
