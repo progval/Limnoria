a861941626a3426b67683316c9ce938d872360c4
Jeremy Fincher <jemfinch@users.sourceforge.net>
2003-04-20 01:02:29 +0000

    Fixed stupid bug.

--- src/MiscCommands.py
+++ src/MiscCommands.py
@@ -59,7 +59,7 @@ class MiscCommands(callbacks.Privmsg):
         else:
             for cb in irc.callbacks:
                 cls = cb.__class__
-                if cls.name().lower().startswith(name) and \
+                if cb.name().lower().startswith(name) and \
                        not issubclass(cls, callbacks.PrivmsgRegexp) and \
                        issubclass(cls, callbacks.Privmsg):
                     commands = [x for x in cls.__dict__
