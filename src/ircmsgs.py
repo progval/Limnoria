d810fe37b0cec3d24f3def770b4ec17c3726f414
Jeremy Fincher <jemfinch@users.sourceforge.net>
2003-06-04 04:45:46 +0000

    TypoError.

--- src/ircmsgs.py
+++ src/ircmsgs.py
@@ -79,7 +79,7 @@ class IrcMsg(object):
         self._user = user
         self._host = host
         self._command = command
-        if self.command == 'NICK':
+        if self.command == 'NICK' and type(args) != tuple:
             args[0] = ircutils.nick(args[0])
         self._args = tuple(args)
             
