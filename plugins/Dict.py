679b8fbaee8de7f06defa88bfbae1cb6fdfb9fbd
Jeremy Fincher <jemfinch@users.sourceforge.net>
2003-09-14 07:39:39 +0000

    Fixed output in the face of a single responding dictionary.

--- plugins/Dict.py
+++ plugins/Dict.py
@@ -136,7 +136,7 @@ class Dict(callbacks.Privmsg):
             s = utils.normalizeWhitespace(s).rstrip(';.,')
             L.append('%s: %s' % (db, s))
         utils.sortBy(len, L)
-        if dictionary == '*':
+        if dictionary == '*' and len(dbs) > 1:
             s = '%s responded: %s' % (utils.commaAndify(dbs), '; '.join(L)) 
         else:
             s = '; '.join(L)
