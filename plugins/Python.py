23d81ec0cd8a835cb0eb49b77de075dc81b261d8
James Vega <jamessan@users.sourceforge.net>
2003-12-01 18:15:53 +0000

    Removed pydoclink (whose idea was it to add that useless command anyways?) /me hides

--- plugins/Python.py
+++ plugins/Python.py
@@ -166,15 +166,6 @@ class Python(callbacks.PrivmsgCommandAndRegexp, plugins.Configurable):
             else:
                 irc.error(msg, 'No function or module %s exists.' % name)
 
-    def pydoclink(self, irc, msg, args):
-        """<python module>
-
-        Returns a link to the current documentation for <python module>
-        """
-        module = privmsgs.getArgs(args)
-        url = 'http://python.org/doc/current/lib/module-%s.html' % module
-        irc.reply(msg, '%s' % url)
-                
     _these = [str(s) for s in this.s.decode('rot13').splitlines() if s]
     _these.pop(0) # Initial line (The Zen of Python...)
     def zen(self, irc, msg, args):
