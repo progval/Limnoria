36b6821c5e934980720748a95a50b83a04fcdfd3
Jeremy Fincher <jemfinch@users.sourceforge.net>
2003-12-18 09:02:12 +0000

    Set the default timeout for all sockets to 10 seconds.

--- src/world.py
+++ src/world.py
@@ -43,11 +43,14 @@ import sre
 import time
 import types
 import atexit
+import socket
 import threading
 
 import log
 import conf
 import ircutils
+
+socket.setdefaulttimeout(10)
 
 startedAt = time.time() # Just in case it doesn't get set later.
 
