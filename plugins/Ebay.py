886e447c8cf24d22fcd6795d7eb89079d497f455
James Vega <jamessan@users.sourceforge.net>
2004-01-20 07:15:37 +0000

    env is your friend

--- plugins/Ebay.py
+++ plugins/Ebay.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.3
+#!/usr/bin/env python
 
 ###
 # Copyright (c) 2003, James Vega
--- plugins/Karma.py
+++ plugins/Karma.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 ###
 # Copyright (c) 2002, Jeremiah Fincher
