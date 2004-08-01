220d4b088965d4f0c081b125ac47728755089aec
Jeremy Fincher <jemfinch@users.sourceforge.net>
2004-08-01 18:10:03 +0000

    Strike--

--- test/test_MoobotFactoids.py
+++ test/test_MoobotFactoids.py
@@ -84,7 +84,7 @@ if sqlite is not None:
             self.assertNotError('who is <reply>$who')
             self.assertResponse('who', 'foo')
             # Check that actions ("\x01ACTION...") don't match
-            self.assertNoResponse('\x01ACTION\x01 is doing something', 3)
+            self.assertNoResponse('\x01ACTION is doing something\x01', 3)
 
         def testLiteral(self):
             self.assertError('literal moo') # no factoids yet
