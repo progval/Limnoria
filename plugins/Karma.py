3af85f301b830f81afadb44d6b576ed3dbcac706
Jeremy Fincher <jemfinch@users.sourceforge.net>
2004-08-11 23:31:18 +0000

    Updated as a precursor to making it database-independent.

--- plugins/Karma.py
+++ plugins/Karma.py
@@ -42,6 +42,7 @@ from itertools import imap
 import supybot.conf as conf
 import supybot.utils as utils
 import supybot.plugins as plugins
+import supybot.ircutils as ircutils
 import supybot.privmsgs as privmsgs
 import supybot.registry as registry
 import supybot.callbacks as callbacks
