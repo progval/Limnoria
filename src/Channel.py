9c64990fcaf8b1d4fcc8484d3da9c55078df3e53
Jeremy Fincher <jemfinch@users.sourceforge.net>
2004-08-19 15:49:32 +0000

    Oops, forgot a state.  It's like seventh grade geography all over again\!

--- src/Channel.py
+++ src/Channel.py
@@ -268,7 +268,7 @@ class Channel(callbacks.Privmsg):
         """
         if self.haveOps(irc, channel, 'kick someone'):
             (nick, reason) = privmsgs.getArgs(args, optional=1)
-            if nick not in irc.channels[channel].users:
+            if nick not in irc.state.channels[channel].users:
                 irc.error('%s isn\'t in %s.' % (nick, channel))
                 return
             if not reason:
