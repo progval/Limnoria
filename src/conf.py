8a352be8ffdbffc9283d6071240e4fdd53560099
Jeremy Fincher <jemfinch@users.sourceforge.net>
2004-08-23 22:07:35 +0000

    Don't ask me why I was copying that list.

--- src/conf.py
+++ src/conf.py
@@ -600,7 +600,7 @@ utils.transactionalFile = transactionalFile
 
 class PluginDirectories(registry.CommaSeparatedListOfStrings):
     def __call__(self):
-        v = registry.CommaSeparatedListOfStrings.__call__(self)[:] # Copy!
+        v = registry.CommaSeparatedListOfStrings.__call__(self)
         if _srcDir not in v:
             v.append(_srcDir)
         if _pluginsDir not in v:
