adedfb827437f613a77616e156e39915200336f4
James Vega <jamessan@users.sourceforge.net>
2004-09-01 18:24:59 +0000

    It helps if I register things correctly

--- plugins/ChannelLogger.py
+++ plugins/ChannelLogger.py
@@ -57,7 +57,7 @@ conf.registerGlobalValue(conf.supybot.plugins.ChannelLogger,
     'flushImmediately', registry.Boolean(False, """Determines whether channel
     logfiles will be flushed anytime they're written to, rather than being
     buffered by the operating system."""))
-conf.registerGlobalValue(conf.supybot.plugins.ChannelLogger,
+conf.registerChannelValue(conf.supybot.plugins.ChannelLogger,
     'stripFormatting', registry.Boolean(True, """Determines whether
     formatting characters (such as bolding, color, etc.) are removed when
     writing the logs to disk."""))
