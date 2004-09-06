87a532374e30c9343ed45b96075a5d7e7df510a6
James Vega <jamessan@users.sourceforge.net>
2004-09-06 17:26:35 +0000

    +cvs

--- src/conf.py
+++ src/conf.py
@@ -50,7 +50,7 @@ _pluginsDir = os.path.join(installDir, 'plugins')
 ###
 # version: This should be pretty obvious.
 ###
-version ='0.79.9999'
+version ='0.79.9999+cvs'
 
 ###
 # *** The following variables are affected by command-line options.  They are
