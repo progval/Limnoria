135c57c31440bf20136d9d3cd0c708d073b3ef19
Jeremy Fincher <jemfinch@users.sourceforge.net>
2004-09-25 16:10:33 +0000

    Let's not try to send empty messages.

--- src/Config.py
+++ src/Config.py
@@ -241,9 +241,15 @@ class Config(callbacks.Privmsg):
         wrapper = getWrapper(name)
         if hasattr(wrapper, 'help'):
             s = wrapper.help
-            if hasattr(wrapper, 'value') and not wrapper._private:
-                s += '  (Current value: %s)' % wrapper
-            irc.reply(s)
+            if s:
+                if hasattr(wrapper, 'value') and not wrapper._private:
+                    s += '  (Current value: %s)' % wrapper
+                irc.reply(s)
+            else:
+                irc.reply('That configuration group exists, but seems to have '
+                          'no help.  Try "config list %s" to see if it has '
+                          'any children values.')
+                
         else:
             irc.error('%s has no help.' % name)
 
