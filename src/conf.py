a982fe32fd9d9b35accce43b8805c7c644d6cf2c
Jeremy Fincher <jemfinch@users.sourceforge.net>
2004-10-02 18:20:04 +0000

    Fix0r the reference to an old undefined variable.

--- src/conf.py
+++ src/conf.py
@@ -464,13 +464,14 @@ registerChannelValue(supybot.replies, 'genericNoCapability',
     This could be because you actually possess the anti-capability for the
     capability that's required of you, or because the channel provides that
     anti-capability by default, or because the global capabilities include
-    that anti-capability.  Or, it could be because the channel or the global
-    defaultAllow is set to False, meaning that no commands are allowed unless
-    explicitly in your capabilities.  Either way, you can't do what you want
-    to do.""", """Determines what generic error message is given when the bot
-    is telling someone that they aren't cool enough to use the command they
-    tried to use, and the author of the code calling errorNoCapability didn't
-    provide an explicit capability for whatever reason."""))
+    that anti-capability.  Or, it could be because the channel or 
+    supybot.capabilities.default is set to False, meaning that no commands are
+    allowed unless explicitly in your capabilities.  Either way, you can't do
+    what you want to do.""",
+    """Determines what generic error message is given when the bot is telling
+    someone that they aren't cool enough to use the command they tried to use,
+    and the author of the code calling errorNoCapability didn't provide an
+    explicit capability for whatever reason."""))
 
 registerChannelValue(supybot.replies, 'requiresPrivacy',
     registry.NormalizedString("""That operation cannot be done in a
