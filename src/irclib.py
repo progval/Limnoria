1e159fb3860e800e35901fbf9705d9dbfcd2518f
James Vega <jamessan@users.sourceforge.net>
2004-11-12 16:25:35 +0000

    Add an XXX

--- src/irclib.py
+++ src/irclib.py
@@ -834,6 +834,7 @@ class Irc(IrcCommandDispatcher):
             L = list(nick)
             while len(L) <= 3:
                 L.append('`')
+            # XXX Should this check ircutils.isNick as well?
             while ret == nick:
                 L[random.randrange(len(L))] = random.choice('0123456789')
                 ret = ''.join(L)
