73a3261cda5afce08320d231b923e8b693ab63fd
James Vega <jamessan@users.sourceforge.net>
2004-12-03 01:57:00 +0000

    Better (?) help message.  Comments/changes welcome.

--- plugins/Alias.py
+++ plugins/Alias.py
@@ -267,9 +267,9 @@ class Alias(callbacks.Privmsg):
         Defines an alias <name> that executes <alias>.  The <alias>
         should be in the standard "command argument [nestedcommand argument]"
         arguments to the alias; they'll be filled with the first, second, etc.
-        arguments.  @1, @2 can be used for optional arguments.  $* simply
-        means "all remaining arguments," and cannot be combined with optional
-        arguments.
+        arguments.  $1, $2, etc. can be used for required arguments.  @1, @2,
+        etc. can be used for optional arguments.  $* simply means "all
+        remaining arguments," and cannot be combined with optional arguments.
         """
         if ' ' not in alias:
             # If it's a single word, they probably want $*.
