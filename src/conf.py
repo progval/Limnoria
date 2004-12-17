360cba9c637de4a32a0b64d7bf00193285175588
James Vega <jamessan@users.sourceforge.net>
2004-12-17 07:33:21 +0000

    Updated to 0.80.0pre4.

--- src/conf.py
+++ src/conf.py
@@ -49,7 +49,7 @@ _pluginsDir = os.path.join(installDir, 'plugins')
 ###
 # version: This should be pretty obvious.
 ###
-version = '0.80.0pre4'
+version = '0.80.0pre4+cvs'
 
 ###
 # *** The following variables are affected by command-line options.  They are
