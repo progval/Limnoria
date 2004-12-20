38814a3da0363af79c1875ba90708b318a9d9953
Jeremy Fincher <jemfinch@users.sourceforge.net>
2004-12-20 07:12:12 +0000

    Updated to 0.80.0pre5.

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ if clean:
             print 'Couldn\'t remove former installation: %s' % e
             sys.exit(-1)
 
-version = '0.80.0pre4+cvs'
+version = '0.80.0pre5'
 setup(
     # Metadata
     name='supybot',
