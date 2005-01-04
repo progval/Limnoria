2a8f2d4a5f282f3299260fd9b5b2186ee3ae97f5
Jeremy Fincher <jemfinch@users.sourceforge.net>
2005-01-04 03:46:06 +0000

    Extra unknown_host thingamajob.

--- src/webutils.py
+++ src/webutils.py
@@ -68,7 +68,7 @@ def strError(e):
         return TIMED_OUT
     elif n == 104:
         return RESET_BY_PEER
-    elif n == 8:
+    elif n in (8, 3):
         return UNKNOWN_HOST
     elif n == 403:
         return FORBIDDEN
