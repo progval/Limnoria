462411590955c18446a6fad9a22d6fb7ffa239ad
Jeremy Fincher <jemfinch@users.sourceforge.net>
2005-01-14 14:46:52 +0000

    No need to makeBackupIfSmaller in databases.  That's more for configuration files.

--- src/dbi.py
+++ src/dbi.py
@@ -262,7 +262,8 @@ class FlatfileMapping(MappingInterface):
 
     def vacuum(self):
         infd = file(self.filename)
-        outfd = utils.transactionalFile(self.filename)
+        outfd = utils.transactionalFile(self.filename,
+                                        makeBackupIfSmaller=False)
         outfd.write(infd.readline()) # First line, nextId.
         for line in infd:
             if not line.startswith('-'):
