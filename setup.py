c961614d9848f397ca8fd0073c2d02cd41c9cdd6
James Vega <jamessan@users.sourceforge.net>
2005-01-17 06:57:37 +0000

    Updated to 0.80.0.

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ if clean:
             print 'Couldn\'t remove former installation: %s' % e
             sys.exit(-1)
 
-version = '0.80.0'
+version = '0.80.0+cvs'
 setup(
     # Metadata
     name='supybot',
