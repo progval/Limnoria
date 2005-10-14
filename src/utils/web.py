9cb1669b5fee5eefed4fe5c3be305727b71046ea
James Vega <jamessan@users.sourceforge.net>
2005-10-14 12:59:47 +0000

    src/utils/web: Add another "UNKNOWN_HOST" code to catch.

--- src/utils/web.py
+++ src/utils/web.py
@@ -66,7 +66,7 @@ def strError(e):
         return TIMED_OUT
     elif n == 104:
         return RESET_BY_PEER
-    elif n in (8, 7, 3, 2, -2):
+    elif n in (8, 7, 3, 2, -2, -3):
         return UNKNOWN_HOST
     elif n == 403:
         return FORBIDDEN
