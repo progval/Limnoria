8786b4c993fbb68f0c02ffc9d2818a3044ace0ae
Jeremy Fincher <jemfinch@users.sourceforge.net>
2008-06-27 19:12:53 +0000

    Added clearq command to clear the send queue.

--- sandbox/Debug/plugin.py
+++ sandbox/Debug/plugin.py
@@ -197,6 +197,14 @@ class Debug(callbacks.Privmsg):
         irc.reply(repr(os.environ))
     environ = wrap(environ)
 
+    def clearq(self, irc, msg, args):
+        """takes no arguments
+
+        Clears the current send queue for this network.
+        """
+        irc.queue.reset()
+        irc.replySuccess()
+
 
 Class = Debug
 
