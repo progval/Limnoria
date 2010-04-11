7f9a1130605fb6b36967f062d3cbcdb73aff8df6
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2010-04-11 00:42:55 -0400

    commit mtughan's bugfix for this bug: https://sourceforge.net/tracker/?func=detail&aid=2985241&group_id=58965&atid=489447

--- src/registry.py
+++ src/registry.py
@@ -307,8 +307,10 @@ class Value(Group):
         if setDefault:
             self.setValue(default)
 
-    def error(self):
-        if self.__doc__:
+    def error(self, message=None):
+        if message:
+            s = message
+        elif self.__doc__:
             s = self.__doc__
         else:
             s = """%s has no docstring.  If you're getting this message,
@@ -539,7 +541,7 @@ class Regexp(Value):
         self.__parent.__init__(*args, **kwargs)
 
     def error(self, e):
-        self.__parent.error('Value must be a regexp of the form %s' % e)
+        self.__parent.error('%s' % e)
 
     def set(self, s):
         try:
