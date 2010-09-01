3a0e19bc225539bfa53b2b9bb953bf6414178878
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2010-09-01 17:37:31 -0400

    Scheduler: remove spammy debug output.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+gribble (2010-08-20T11:08:31-0400)'
+version = '0.83.4.1+gribble (2010-09-02T08:54:13-0400)'
