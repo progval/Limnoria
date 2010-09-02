567e72488d4ee68917e27f265adf31e25f775672
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2010-09-02 09:14:22 -0400

    Cherry-pick a bunch of upstream fix commits.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+gribble (2010-09-02T08:54:13-0400)'
+version = '0.83.4.1+gribble (2010-09-02T09:14:22-0400)'
