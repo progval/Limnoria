4b1d2b49e4375d91b2065f043d98d7beb81aeb8f
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2010-09-04 01:25:57 -0400

    Scheduler: don't immediately execute commands when restoring repeated events.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-07-06T11:14:57+0200)'
+version = '0.83.4.1+limnoria (2011-07-07T13:31:50+0200)'
