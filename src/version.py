8b702543faac20af92a3151af5acfa6463c7f458
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2010-10-10 17:52:04 -0400

    update the version timestamp for the batch of upstream bugfixes that just came through.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+gribble (2010-09-08T00:11:28-0400)'
+version = '0.83.4.1+gribble (2010-10-10T17:52:04-0400)'
