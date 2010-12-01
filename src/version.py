42464d8180361b83ef090119a5e6f9ed32c8d142
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2010-12-01 16:52:01 -0500

    Google: add some extra matching capability to google calc now should be able to display any 'special' result from google.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+gribble (2010-10-10T17:52:04-0400)'
+version = '0.83.4.1+gribble (2010-12-01T16:53:08-0500)'
