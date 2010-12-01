7c7b093b33de3be88782a3d655a64f7a327f28c9
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2010-12-01 16:52:01 -0500

    Google: add some extra matching capability to google calc now should be able to display any 'special' result from google.

--- plugins/Google/test.py
+++ plugins/Google/test.py
@@ -39,6 +39,7 @@ class GoogleTestCase(ChannelPluginTestCase):
         def testCalc(self):
             self.assertNotRegexp('google calc e^(i*pi)+1', r'didn\'t')
             self.assertNotRegexp('google calc 1 usd in gbp', r'didn\'t')
+            self.assertRegexp('google calc current time in usa', r'Time in.*USA')
 
         def testHtmlHandled(self):
             self.assertNotRegexp('google calc '
