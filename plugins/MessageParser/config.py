7e1cd95f46475d0dd6747caf8ccc5c24493b5a96
Valentin Lorentz <progval@gmail.com>
2011-03-01 14:49:47 +0100

    MessageParser: fix missing configure() in config.py

--- plugins/MessageParser/config.py
+++ plugins/MessageParser/config.py
@@ -41,6 +41,14 @@ except:
     _ = lambda x:x
     internationalizeDocstring = lambda x:x
 
+def configure(advanced):
+    # This will be called by supybot to configure this module.  advanced is
+    # a bool that specifies whether the user identified himself as an advanced
+    # user or not.  You should effect your configuration by manipulating the
+    # registry as appropriate.
+    from supybot.questions import expect, anything, something, yn
+    conf.registerPlugin('MessageParser', True)
+
 MessageParser = conf.registerPlugin('MessageParser')
 # This is where your configuration variables (if any) should go.  For example:
 # conf.registerGlobalValue(MessageParser, 'someConfigVariableName',
