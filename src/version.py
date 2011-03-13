c25db0ecdfdf91b8fa8183fc6cf1dcebf7432a3a
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2011-03-13 14:21:46 -0400

    Services: fix problem with some channels being mixed up between networks on startup, when noJoinsUntilIdentified is true.
    When noJoinsUntilIdentified config is true, the bot holds join messages in a 'waitingJoins' list, and processes them
    once nickserv identification comes through. The problem was that when the bot is configured to join multiple networks,
    join messages from different networks would get appended to the same list, without any differentiation by which message
    belongs to which network. Thus, if there are messages waiting for multiple networks, it would often be the case that
    whichever network got identification done first, would 'pick up' other network's join messages.
    
    This fix stores the network name along with the join messages in the list, and has each network pick out only its own
    join messages.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+gribble (2010-12-01T16:53:08-0500)'
+version = '0.83.4.1+gribble (2011-03-13T14:21:46-0400)'
