7d402e8265dfd6519d06a30ea6f2e137d47a0e96
Valentin Lorentz <progval@gmail.com>
2011-05-14 16:36:25 +0200

    Minecraft support: replace esbot by Bravo as a library.

--- src/drivers/Twisted.py
+++ src/drivers/Twisted.py
@@ -35,10 +35,15 @@ import supybot.drivers as drivers
 import supybot.ircmsgs as ircmsgs
 from supybot.minecraftformat import DataBuffer, IncompleteDataError
 import supybot.minecraftprotocol as minecraftprotocol
+import supybot.minecraftmsgs as minecraftmsgs
+
+from bravo.location import Location
+from bravo.packets.beta import parse_packets, make_packet, make_error_packet
 
 from twisted.names import client
 from twisted.internet import reactor, error
 from twisted.protocols.basic import LineReceiver
+from twisted.internet.task import cooperate, deferLater, LoopingCall
 from twisted.internet.protocol import ReconnectingClientFactory, Protocol
 
 
@@ -112,70 +117,309 @@ class SupyIrcProtocol(LineReceiver):
         self.transport.loseConnection()
 
 class SupyMcProtocol(Protocol):
+    # From bravo server
+    (STATE_UNAUTHENTICATED, STATE_CHALLENGED, STATE_AUTHENTICATED) = range(3)
+
+    SUPPORTED_PROTOCOL = 11
+
+    excess = ""
+    packet = None
+
+    state = STATE_UNAUTHENTICATED
+
+    buf = ""
+    parser = None
+    handler = None
+
+    player = None
+    username = None
+
     def __init__(self):
-        self.buffer = ''
-        self.mostRecentCall = reactor.callLater(0.1, self.checkIrcForMsgs)
+        self.chunks = dict()
+        self.windows = dict()
+        self.wid = 1
 
-    def dataReceived(self, data):
-        # From https://github.com/espes/esbot/blob/master/MCProtocol.py
-        self.buffer += data
+        self.location = Location()
 
-        parseBuffer = DataBuffer(self.buffer)
-        while parseBuffer.lenLeft() > 0:
-            positionBeforeRead = parseBuffer.tell()
-            packetType = ord(parseBuffer.read(1))
+        self.handlers = {
+            0: self.ping,
+            1: self.login,
+            2: self.handshake,
+            3: self.chat,
+            7: self.use,
+            10: self.grounded,
+            11: self.position,
+            12: self.orientation,
+            13: self.location_packet,
+            14: self.digging,
+            15: self.build,
+            16: self.equip,
+            18: self.animate,
+            19: self.action,
+            21: self.pickup,
+            101: self.wclose,
+            102: self.waction,
+            104: self.inventory,
+            106: self.wacknowledge,
+            130: self.sign,
+            255: self.quit,
+        }
 
-            try:
-                packetClass = minecraftprotocol.idToPacket[packetType]
-            except KeyError:
-                log.error('Got unknown packet id %i' % packetType)
-                self.transport.loseConnection()
-                return
 
-            try:
-                packet = packetClass(parseBuffer)
-            except IncompleteDataError:
-                parseBuffer.seek(positionBeforeRead)
-                break
-            self.buffer = parseBuffer.peek()
+        self._ping_loop = LoopingCall(self.update_ping)
 
-            msg = ircmsgs.IrcMsg(s='', command='MINECRAFT',
-                    args=(self.irc.nick, packet))
-            self.irc.feedMsg(msg)
+    # Low-level packet handlers
+    # Try not to hook these if possible, since they offer no convenient
+    # abstractions or protections.
 
-    def checkIrcForMsgs(self):
-        if self.connected:
-            msg = self.irc.takeMsg()
-            while msg:
-                if msg.command == 'MINECRAFT':
-                    msg = msg.args[0]
-                else:
-                    msg = minecraftprotocol.ircToMinecraft(msg)
-                assert msg is not None
-                self.transport.write(str(msg))
-                msg = self.irc.takeMsg()
-        self.mostRecentCall = reactor.callLater(0.1, self.checkIrcForMsgs)
-
-    def connectionLost(self, r):
-        self.mostRecentCall.cancel()
-        if r.check(error.ConnectionDone):
-            drivers.log.disconnect(self.factory.currentServer)
-        else:
-            drivers.log.disconnect(self.factory.currentServer, errorMsg(r))
-        if self.irc.zombie:
-            self.factory.stopTrying()
-            while self.irc.takeMsg():
-                continue
-        else:
-            self.irc.reset()
+    def ping(self, container):
+        """
+        Hook for ping packets.
+        """
 
     def connectionMade(self):
-        self.factory.resetDelay()
-        self.irc.driver = self
+        packet = make_packet("login", protocol=11, username='test', seed=0,
+            dimension=0)
+        self.transport.write(packet)
 
-    def die(self):
-        drivers.log.die(self.irc)
-        self.factory.stopTrying()
+    def login(self, container):
+        """
+        Hook for login packets.
+
+        Override this to customize how logins are handled. By default, this
+        method will only confirm that the negotiated wire protocol is the
+        correct version, and then it will run the ``authenticated()``
+        callback.
+        """
+
+        if container.protocol < self.SUPPORTED_PROTOCOL:
+            # Kick old clients.
+            self.error("This server doesn't support your ancient client.")
+        elif container.protocol > self.SUPPORTED_PROTOCOL:
+            # Kick new clients.
+            self.error("This server doesn't support your newfangled client.")
+        else:
+            reactor.callLater(0, self.authenticated)
+
+    def handshake(self, container):
+        """
+        Hook for handshake packets.
+        """
+
+    def chat(self, container):
+        """
+        Hook for chat packets.
+        """
+
+    def use(self, container):
+        """
+        Hook for use packets.
+        """
+
+    def grounded(self, container):
+        """
+        Hook for grounded packets.
+        """
+
+        self.location.grounded = bool(container.grounded)
+
+    def position(self, container):
+        """
+        Hook for position packets.
+        """
+
+        old_position = self.location.x, self.location.y, self.location.z
+
+        # Location represents the block the player is within
+        self.location.x = int(container.position.x) if container.position.x > 0 else int(container.position.x) - 1
+        self.location.y = int(container.position.y)
+        self.location.z = int(container.position.z) if container.position.z > 0 else int(container.position.z) - 1
+        # Stance is the current jumping position, plus a small offset of
+        # around 0.1. In the Alpha server, it must between 0.1 and 1.65,
+        # or the anti-grounded code kicks the client.
+        self.location.stance = container.position.stance
+
+        position = self.location.x, self.location.y, self.location.z
+
+        self.grounded(container.grounded)
+
+        if old_position != position:
+            self.position_changed()
+
+    def orientation(self, container):
+        """
+        Hook for orientation packets.
+        """
+
+        old_orientation = self.location.yaw, self.location.pitch
+
+        self.location.yaw = container.orientation.rotation
+        self.location.pitch = container.orientation.pitch
+
+        orientation = self.location.yaw, self.location.pitch
+
+        self.grounded(container.grounded)
+
+        if old_orientation != orientation:
+            self.orientation_changed()
+
+    def location_packet(self, container):
+        """
+        Hook for location packets.
+        """
+
+        self.position(container)
+        self.orientation(container)
+
+    def digging(self, container):
+        """
+        Hook for digging packets.
+        """
+
+    def build(self, container):
+        """
+        Hook for build packets.
+        """
+
+    def equip(self, container):
+        """
+        Hook for equip packets.
+        """
+
+    def pickup(self, container):
+        """
+        Hook for pickup packets.
+        """
+
+    def animate(self, container):
+        """
+        Hook for animate packets.
+        """
+
+    def action(self, container):
+        """
+        Hook for action packets.
+        """
+
+    def wclose(self, container):
+        """
+        Hook for wclose packets.
+        """
+
+    def waction(self, container):
+        """
+        Hook for waction packets.
+        """
+
+    def inventory(self, container):
+        """
+        Hook for inventory packets.
+        """
+
+    def wacknowledge(self, container):
+        """
+        Hook for wacknowledge packets.
+        """
+
+    def sign(self, container):
+        """
+        Hook for sign packets.
+        """
+
+    def quit(self, container):
+        """
+        Hook for quit packets.
+
+        By default, merely logs the quit message and drops the connection.
+
+        Even if the connection is not dropped, it will be lost anyway since
+        the client will close the connection. It's better to explicitly let it
+        go here than to have zombie protocols.
+        """
+
+        log.msg("Client is quitting: %s" % container.message)
+        self.transport.loseConnection()
+
+    # Twisted-level data handlers and methods
+    # Please don't override these needlessly, as they are pretty solid and
+    # shouldn't need to be touched.
+
+    def dataReceived(self, data):
+        self.buf += data
+
+        packets, self.buf = parse_packets(self.buf)
+
+        for header, payload in packets:
+            if header in self.handlers:
+                log.debug('Packet: %i' % header)
+                self.handlers[header](payload)
+            else:
+                log.error("Didn't handle parseable packet %d!" % header)
+                log.error(repr(payload)[0:500]) # Truncate chunk packets
+
+    def connectionLost(self, reason):
+        if self._ping_loop.running:
+            self._ping_loop.stop()
+
+    # State-change callbacks
+    # Feel free to override these, but call them at some point.
+
+    def challenged(self):
+        """
+        Called when the client has started authentication with the server.
+        """
+
+        self.state = STATE_CHALLENGED
+
+    def authenticated(self):
+        """
+        Called when the client has successfully authenticated with the server.
+        """
+
+        self.state = STATE_AUTHENTICATED
+
+        self._ping_loop.start(5)
+
+    # Event callbacks
+    # These are meant to be overriden.
+
+    def orientation_changed(self):
+        """
+        Called when the client moves.
+
+        This callback is only for orientation, not position.
+        """
+
+        pass
+
+    def position_changed(self):
+        """
+        Called when the client moves.
+
+        This callback is only for position, not orientation.
+        """
+
+        pass
+
+    # Convenience methods
+
+    def update_ping(self):
+        """
+        Send a keepalive to the client.
+        """
+
+        packet = make_packet("ping")
+        self.transport.write(packet)
+
+    def error(self, message):
+        """
+        Error out.
+
+        This method sends ``message`` to the client as a descriptive error
+        message, then closes the connection.
+        """
+
+        self.transport.write(make_error_packet(message))
         self.transport.loseConnection()
 
 def errorMsg(reason):
