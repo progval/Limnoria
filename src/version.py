a7f6f6c3b3fde596e11dd4d2cd52d5eb26265603
Valentin Lorentz <progval@gmail.com>
2011-06-25 15:15:19 +0200

    Update the version string

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-06-23T12:30:23+0200)'
+version = '0.83.4.1+limnoria (2011-06-25T15:15:16+0200)'
