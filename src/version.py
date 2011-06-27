1e337bdfa9085133ddd54c82cffa69dfcaa13c9a
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2011-06-27 14:41:49 -0400

    Dict: Fix FSF address in included dictclient module
    Taking fresh address from http://www.gnu.org/licenses/old-licenses/gpl-2.0.html#SEC4

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+gribble (2011-04-04T16:30:52-0400)'
+version = '0.83.4.1+gribble (2011-06-27T14:41:49-0400)'
