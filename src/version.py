0dfdfa2d280eb074e8d0e6265f71e4c886835237
Valentin Lorentz <progval@gmail.com>
2011-07-09 14:05:28 +0200

    Channel: fix NameError: 'replyirc' -> 'replyIrc'. Closes GH-73.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-07-09T13:44:16+0200)'
+version = '0.83.4.1+limnoria (2011-07-09T14:05:28+0200)'
