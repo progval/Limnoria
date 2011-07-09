c06b86b13cd930899655f1faf873a7a76cfb1d8c
Valentin Lorentz <progval@gmail.com>
2011-07-09 13:42:34 +0200

    Bug fixes in src/ircmsgs.py
    unbans did send the repr() of the ban list, and IrcMsg.__hash__ did try to hash a list.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-07-08T10:41:36+0200)'
+version = '0.83.4.1+limnoria (2011-07-09T13:42:34+0200)'
