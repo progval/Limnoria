4edfed5ed7da16e0ca95effed3b59f112e65abab
Valentin Lorentz <progval@gmail.com>
2011-07-12 16:09:55 +0200

    AutoMode: fix previous commit.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-07-12T15:04:13+0200)'
+version = '0.83.4.1+limnoria (2011-07-12T16:09:54+0200)'
