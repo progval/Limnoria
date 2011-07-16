03cc4c7ab7dbb576298f8896c603f4a937786059
Valentin Lorentz <progval@gmail.com>
2011-07-16 13:37:56 +0200

    supybot-wizard: Suggest most useful plugins when the user want to add plugins en masse. Closes GH-84.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-07-14T17:34:26+0200)'
+version = '0.83.4.1+limnoria (2011-07-16T13:37:56+0200)'
