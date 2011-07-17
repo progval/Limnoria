a4c0a13b1d69e3595c67f09a5667a82640922321
skizzhg <skizzhg@gmx.com>
2011-07-17 15:21:07 +0200

    MessageParser: Fix typo (rexeps -> regexps).

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-07-16T14:27:52+0200)'
+version = '0.83.4.1+limnoria (2011-07-17T15:21:07+0200)'
