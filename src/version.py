083815c6f7b6b386c04e53ef8a015aeba20bbbad
Valentin Lorentz <progval@gmail.com>
2011-07-18 15:23:06 +0200

    Factoids: Fix typo.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-07-17T15:21:07+0200)'
+version = '0.83.4.1+limnoria (2011-07-18T15:23:06+0200)'
