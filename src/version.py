7e88bb1ce3ae5d548ed982296ae7613dc0957f9d
skizzhg <skizzhg@gmx.com>
2011-08-07 11:16:04 +0200

    Update l10n-it for many plugins.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-08-06T18:57:56+0200)'
+version = '0.83.4.1+limnoria (2011-08-07T11:16:04+0200)'
