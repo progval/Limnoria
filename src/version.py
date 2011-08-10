cf436631d686495db801ff03838cc7458a892169
skizzhg <skizzhg@gmx.com>
2011-08-10 11:48:07 +0200

    Admin & Anonymous & AutoMode & Channel & Misc & NickCapture & Seen & Todo & URL: Update l10n-it.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-08-10T11:44:57+0200)'
+version = '0.83.4.1+limnoria (2011-08-10T11:48:07+0200)'
