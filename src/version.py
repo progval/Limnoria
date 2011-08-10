54aef4f3d5504ad93eec564b40b0b5f671d60bdd
skizzhg <skizzhg@gmx.com>
2011-08-10 17:23:59 +0200

    Later & Misc & Seen & Todo: Update l10n-it.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-08-10T12:00:42+0200)'
+version = '0.83.4.1+limnoria (2011-08-10T17:23:59+0200)'
