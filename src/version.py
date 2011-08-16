22eb14711656ebee18c320cfbacbf202bc7d63fc
Valentin Lorentz <progval@gmail.com>
2011-08-16 13:22:23 +0200

    Channel: Really deny access to the nicks list. Closes GH-172.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-08-15T17:24:33+0200)'
+version = '0.83.4.1+limnoria (2011-08-16T13:22:23+0200)'
