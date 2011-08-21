f37db99895f9ac83d9a4741c2247cbcd89c7338b
skizzhg <skizzhg@gmx.com>
2011-08-21 20:49:22 +0200

    Utilities: Add l10n-it.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-08-18T11:07:25+0200)'
+version = '0.83.4.1+limnoria (2011-08-21T20:49:22+0200)'
