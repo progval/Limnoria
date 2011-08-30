2a3fc2a0f3177d72b2053b3ec2173107c3873d6f
skizzhg <skizzhg@gmx.com>
2011-08-30 17:21:39 +0000

    ChannelStats: Add l10n-it.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-08-27T13:58:14+0000)'
+version = '0.83.4.1+limnoria (2011-08-30T17:21:39+0000)'
