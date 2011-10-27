a87c096d69d8ac0ed693fa07d8bc83e5006875ca
Valentin Lorentz <progval@gmail.com>
2011-10-27 10:47:59 +0200

    Allow plugins to take over a subdirectory of the HTTP server.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-10-27T10:47:25+0200)'
+version = '0.83.4.1+limnoria (2011-10-27T10:47:59+0200)'
