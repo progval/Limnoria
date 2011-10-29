progval/Limnoria

commit 543e78828b258fe764017e95ebdd30d39fd74555

Add utils.net.isIPV4, with utils.net.isIP checking v4 or v6
Signed-off-by: James Vega <jamessan@users.sourceforge.net>

src/utils/net.py
@@ -1,5 +1,6 @@
 ###
 # Copyright (c) 2002-2005, Jeremiah Fincher
+# Copyright (c) 2011, James Vega
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -44,7 +45,7 @@ def getSocket(host):
     """
     addrinfo = socket.getaddrinfo(host, None)
     host = addrinfo[0][4][0]
-    if isIP(host):
+    if isIPV4(host):
         return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     elif isIPV6(host):
         return socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
@@ -52,16 +53,27 @@ def getSocket(host):
         raise socket.error, 'Something wonky happened.'
 
 def isIP(s):
-    """Returns whether or not a given string is an IPV4 address.
+    """Returns whether or not a given string is an IP address.
 
     >>> isIP('255.255.255.255')
     1
 
-    >>> isIP('abc.abc.abc.abc')
+    >>> isIP('::1')
+    0
+    """
+    return isIPV4(s) or isIPV6(s)
+
+def isIPV4(s):
+    """Returns whether or not a given string is an IPV4 address.
+
+    >>> isIPV4('255.255.255.255')
+    1
+
+    >>> isIPV4('abc.abc.abc.abc')
     0
     """
     try:
-        return bool(socket.inet_aton(s))
+        return bool(socket.inet_pton(socket.AF_INET, s))
     except socket.error:
         return False
 

src/conf.py
@@ -1,6 +1,6 @@
 ###
 # Copyright (c) 2002-2005, Jeremiah Fincher
-# Copyright (c) 2008-2009, James Vega
+# Copyright (c) 2008-2009,2011, James Vega
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -1031,7 +1031,7 @@ registerGlobalValue(supybot, 'defaultIgnore',
 class IP(registry.String):
     """Value must be a valid IP."""
     def setValue(self, v):
-        if v and not (utils.net.isIP(v) or utils.net.isIPV6(v)):
+        if v and not utils.net.isIP(v):
             self.error()
         else:
             registry.String.setValue(self, v)

src/ircutils.py
@@ -1,5 +1,6 @@
 ###
 # Copyright (c) 2002-2005, Jeremiah Fincher
+# Copyright (c) 2011, James Vega
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -190,7 +191,7 @@ def banmask(hostmask):
     """
     assert isUserHostmask(hostmask)
     host = hostFromHostmask(hostmask)
-    if utils.net.isIP(host):
+    if utils.net.isIPV4(host):
         L = host.split('.')
         L[-1] = '*'
         return '*!*@' + '.'.join(L)
@@ -461,8 +462,8 @@ def replyTo(msg):
         return msg.nick
 
 def dccIP(ip):
-    """Returns in IP in the proper for DCC."""
-    assert utils.net.isIP(ip), \
+    """Returns an IP in the proper for DCC."""
+    assert utils.net.isIPV4(ip), \
            'argument must be a string ip in xxx.yyy.zzz.www format.'
     i = 0
     x = 256**3

test/test_utils.py
@@ -1,6 +1,6 @@
 ###
 # Copyright (c) 2002-2005, Jeremiah Fincher
-# Copyright (c) 2009, James Vega
+# Copyright (c) 2009,2011, James Vega
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -510,11 +510,9 @@ class NetTest(SupyTestCase):
         isIP = utils.net.isIP
         self.failIf(isIP('a.b.c'))
         self.failIf(isIP('256.0.0.0'))
-        self.failUnless(isIP('127.1'))
         self.failUnless(isIP('0.0.0.0'))
         self.failUnless(isIP('100.100.100.100'))
-        # This test is too flaky to bother with.
-        # self.failUnless(utils.isIP('255.255.255.255'))
+        self.failUnless(isIP('255.255.255.255'))
 
     def testIsIPV6(self):
         f = utils.net.isIPV6

commit 08405c6ec5c6cc7fd3b4a3ef99a90e1bc1eb1c8f

Fix Windows support (broken in 543e7882).

src/utils/net.py
@@ -114,7 +114,10 @@ def isIPV4(s):
     0
     """
     try:
-        return bool(socket.inet_pton(socket.AF_INET, s))
+        try:
+            return bool(socket.inet_pton(socket.AF_INET, s))
+        except AttributeError: # Running Windows.
+            return bool(socket.inet_aton(s))
     except socket.error:
         return False
 

src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-10-29T13:46:10+0200)'
+version = '0.83.4.1+limnoria (2011-10-29T14:46:35+0200)'
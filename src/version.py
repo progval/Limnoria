08405c6ec5c6cc7fd3b4a3ef99a90e1bc1eb1c8f
Valentin Lorentz <progval@gmail.com>
2011-10-29 14:46:35 +0200

    Fix Windows support (broken in 543e7882).

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-10-29T13:46:10+0200)'
+version = '0.83.4.1+limnoria (2011-10-29T14:46:35+0200)'
