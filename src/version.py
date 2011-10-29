8b616d2f5965efd26528ce342a2dd85cc15d01db
Valentin Lorentz <progval@gmail.com>
2011-10-29 17:10:58 +0200

    RSS: update l10n-de l10n-fi l10n-it. fix l10n-fr.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-10-29T16:52:29+0200)'
+version = '0.83.4.1+limnoria (2011-10-29T17:10:58+0200)'
