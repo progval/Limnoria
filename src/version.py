a93750bad720516d06a71f374e48bb01a2609115
skizzhg <skizzhg@gmx.com>
2011-11-07 19:40:04 +0100

    Admin: Update l10n-it.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-10-30T19:22:39+0100)'
+version = '0.83.4.1+limnoria (2011-11-07T19:40:04+0100)'
