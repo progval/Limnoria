348d1bd99c0f0b9801d53f7b3c2567ef7e069362
Valentin Lorentz <progval@gmail.com>
2011-11-11 19:56:20 +0100

    Fix typo (lamda => lambda).

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-11-07T19:40:04+0100)'
+version = '0.83.4.1+limnoria (2011-11-11T19:56:20+0100)'
