332420bad953fcb64b487618bf3c402546f6207c
Valentin Lorentz <progval@gmail.com>
2011-12-06 17:14:28 +0100

    Fix previous merge.
    Conflicts:
    
    	src/drivers/Socket.py
    	src/version.py

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-12-03T12:45:07+0100)'
+version = '0.83.4.1+limnoria (2011-12-06T17:14:28+0100)'
