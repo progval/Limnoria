3db9568b71ecd56256bd9ac349e6a6d97ad1a375
nyuszika7h <litemininyuszika@gmail.com>
2012-01-06 13:25:08 +0100

    sandbox/update-version: Simplify logic in updating version while still using UTC time.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2011-12-06T17:42:04+0100)'
+version = '0.83.4.1+limnoria (2012-01-06T12:23:09+0000)'
