5c9a62e69ae54149247e495efecfa4dfd134b1ad
Valentin Lorentz <progval@gmail.com>
2012-02-11 15:18:32 +0100

    Channel: Fix docstring. Closes GH-129.

--- plugins/Channel/plugin.py
+++ plugins/Channel/plugin.py
@@ -618,8 +618,8 @@ class Channel(callbacks.Plugin):
         def add(self, irc, msg, args, channel, user, capabilities):
             """[<channel>] <nick|username> <capability> [<capability> ...]
 
-            If you have the #channel,op capability, this will give the user
-            <name> (or the user to whom <nick> maps)
+            If you have the #channel,op capability, this will give the
+            <username> (or the user to whom <nick> maps)
             the capability <capability> in the channel. <channel> is only
             necessary if the message isn't sent in the channel itself.
             """
