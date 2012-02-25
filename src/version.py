99ad2d193e41bff25882c27c0df39f8e12125c13
Valentin Lorentz <progval@gmail.com>
2012-02-25 14:02:02 +0100

    BadWords: Fix commit 123c17663732e0130fcf2b3fb53e949667aca4cf.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2012-02-17T21:59:23+0000)'
+version = '0.83.4.1+limnoria (2012-02-25T13:02:02+0000)'
