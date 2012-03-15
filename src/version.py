a5e8f8ede83073f32156d0a58f0756d728142362
Valentin Lorentz <progval@gmail.com>
2012-03-15 20:22:46 +0100

    Allow supybot.directories.backup to be /dev/null.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2012-03-15T12:44:43+0000)'
+version = '0.83.4.1+limnoria (2012-03-15T19:22:46+0000)'
