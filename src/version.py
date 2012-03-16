aedfc07762da87f0c55e42c9234e982383d43e2f
skizzhg <skizzhg@gmx.com>
2012-03-16 18:28:05 +0100

    Update l10n-it.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2012-03-15T19:22:46+0000)'
+version = '0.83.4.1+limnoria (2012-03-16T17:28:05+0000)'
