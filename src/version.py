11417cea8394760251258922fdaa1b409bd05a0b
Mika Suomalainen <mika.henrik.mainio@hotmail.com>
2012-03-16 18:35:14 +0100

    Core: Update l10n-fi.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2012-03-16T17:28:05+0000)'
+version = '0.83.4.1+limnoria (2012-03-16T17:35:14+0000)'
