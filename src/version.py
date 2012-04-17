9d6b132233d744cdc736c853f1cd730494b5f44b
Valentin Lorentz <progval@progval.net>
2012-04-17 11:45:25 +0200

    i18n: Fix issue with the bot sending the repr() of the string on IRC when translation has 'new line' characters in it.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2012-04-15T12:02:44+0000)'
+version = '0.83.4.1+limnoria (2012-04-17T09:45:25+0000)'
