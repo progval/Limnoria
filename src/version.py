bd6ff971c9e128c37cc7b15c0929cacc7bfc99f5
Valentin Lorentz <progval@progval.net>
2012-04-19 21:31:21 +0200

    Factoids & Format & Praise: fix l10n-fi and l10n-it.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2012-04-17T09:49:03+0000)'
+version = '0.83.4.1+limnoria (2012-04-19T19:31:21+0000)'
