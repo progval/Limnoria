c83481bd9c0aec34591ddfd62e2e597f06182ace
Valentin Lorentz <progval@progval.net>
2012-05-02 21:06:22 +0200

    Use self instead of non-existing variable name.

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 """stick the various versioning attributes in here, so we only have to change
 them once."""
-version = '0.83.4.1+limnoria (2012-05-02T18:58:56+0000)'
+version = '0.83.4.1+limnoria (2012-05-02T19:06:22+0000)'
