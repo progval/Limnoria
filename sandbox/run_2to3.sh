c149e8fa816178a39c864cd178becaf761ba00a5
Valentin Lorentz <progval@progval.net>
2012-08-05 17:56:25 +0200

    Make 2to3 ouput files in py3k/ subfolder.

--- sandbox/run_2to3.sh
+++ sandbox/run_2to3.sh
@@ -1,1 +1,2 @@
-python 2to3/run.py src/ plugins/ test/ scripts/* setup.py -wn -f all -f def_iteritems -f def_itervalues -f def_iterkeys -f reload "$@"
+python 2to3/run.py src/ plugins/ test/ scripts/* setup.py -wWno py3k -f all -f def_iteritems -f def_itervalues -f def_iterkeys -f reload "$@"
+cp locale/ py3k -R
