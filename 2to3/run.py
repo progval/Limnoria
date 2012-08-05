d4f5c03b3f1f99643c5fd11da881727ec34aa07d
Valentin Lorentz <progval@progval.net>
2012-08-05 13:28:09 +0200

    Add 2to3/fix_reload.py.

--- 2to3/run.py
+++ 2to3/run.py
@@ -2,11 +2,12 @@
 import sys
 from lib2to3.main import main
 
-import fix_def_iteritems, fix_def_itervalues, fix_def_iterkeys
+import fix_def_iteritems, fix_def_itervalues, fix_def_iterkeys, fix_reload
 
 # Hacks
 sys.modules['lib2to3.fixes.fix_def_iteritems'] = fix_def_iteritems
 sys.modules['lib2to3.fixes.fix_def_itervalues'] = fix_def_itervalues
 sys.modules['lib2to3.fixes.fix_def_iterkeys'] = fix_def_iterkeys
+sys.modules['lib2to3.fixes.fix_reload'] = fix_reload
 
 sys.exit(main("lib2to3.fixes"))
--- sandbox/run_2to3.sh
+++ sandbox/run_2to3.sh
@@ -1,1 +1,1 @@
-python 2to3/run.py src/ plugins/ test/ scripts/* setup.py -wn -f all -f def_iteritems -f def_itervalues -f def_iterkeys "$@"
+python 2to3/run.py src/ plugins/ test/ scripts/* setup.py -wn -f all -f def_iteritems -f def_itervalues -f def_iterkeys -f reload "$@"
