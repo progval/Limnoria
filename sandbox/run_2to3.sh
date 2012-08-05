fad0c9d78868fcbe357bfe8cd80218f82f98f2df
Valentin Lorentz <progval@progval.net>
2012-08-05 22:20:25 +0200

    Copy localised files in run_2to3.sh.

--- sandbox/run_2to3.sh
+++ sandbox/run_2to3.sh
@@ -1,2 +1,3 @@
+cp locale/ py3k/ -R
+cp plugins/ py3k/ -R # copy plugins data
 python 2to3/run.py src/ plugins/ test/ scripts/* setup.py -wWno py3k -f all -f def_iteritems -f def_itervalues -f def_iterkeys -f reload "$@"
-cp locale/ py3k -R
