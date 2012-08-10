12b8e44dc78b423d6b07a9f16be98feea61013bd
Valentin Lorentz <progval@gmail.com>
2012-08-10 13:04:25 +0000

    AutoMode: Fix traceback since b5c99b1c.

--- plugins/AutoMode/plugin.py
+++ plugins/AutoMode/plugin.py
@@ -52,19 +52,22 @@ class AutoMode(callbacks.Plugin):
         fallthrough = self.registryValue('fallthrough', channel)
         def do(type):
             cap = ircdb.makeChannelCapability(channel, type)
-            if ircdb.checkCapability(msg.prefix, cap,
-                    ignoreOwner=not self.registryValue('owner')):
-                if self.registryValue(type, channel):
-                    self.log.info('Scheduling auto-%s of %s in %s.',
-                                  type, msg.prefix, channel)
-                    msgmaker = getattr(ircmsgs, type)
-                    schedule_msg(msgmaker(channel, msg.nick))
-                    raise Continue # Even if fallthrough, let's only do one.
-                elif not fallthrough:
-                    self.log.debug('%s has %s, but supybot.plugins.AutoMode.%s'
-                                   ' is not enabled in %s, refusing to fall '
-                                   'through.', msg.prefix, cap, type, channel)
-                    raise Continue
+            try:
+                if ircdb.checkCapability(msg.prefix, cap,
+                        ignoreOwner=not self.registryValue('owner')):
+                    if self.registryValue(type, channel):
+                        self.log.info('Scheduling auto-%s of %s in %s.',
+                                      type, msg.prefix, channel)
+                        msgmaker = getattr(ircmsgs, type)
+                        schedule_msg(msgmaker(channel, msg.nick))
+                        raise Continue # Even if fallthrough, let's only do one.
+                    elif not fallthrough:
+                        self.log.debug('%s has %s, but supybot.plugins.AutoMode.%s'
+                                       ' is not enabled in %s, refusing to fall '
+                                       'through.', msg.prefix, cap, type, channel)
+                        raise Continue
+            except KeyError:
+                pass
         def schedule_msg(msg):
             def f():
                 irc.queueMsg(msg)
