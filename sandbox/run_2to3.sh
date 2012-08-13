a7d36297c2e20537ade9621f7dc22f478a722182
Valentin Lorentz <progval@gmail.com>
2012-08-13 12:37:23 +0000

    Prevent run_2to3 from copying locale/ to py3k/ instead of py3k/locale/ (thanks Cravix).

--- sandbox/run_2to3.sh
+++ sandbox/run_2to3.sh
@@ -1,4 +1,6 @@
 rm -f src/version.py # Prevent 2to3 from copying it, since py3k/src/version.py was probably written by root.
+rm -rf py3k
+mkdir py3k
 cp locale/ py3k/ -R
 cp plugins/ py3k/ -R # copy plugins data
 python 2to3/run.py src/ plugins/ test/ scripts/* setup.py -wWno py3k -f all -f def_iteritems -f def_itervalues -f def_iterkeys -f reload "$@"
