653e4dec1b4df2d2e31c09fc0950eacfed18ebf2
Joe Julian <me@joejulian.name>
2012-08-13 11:47:43 -0700

    urlquoting the url leads to short urls that prepend "http://" to the url

--- plugins/ShrinkUrl/plugin.py
+++ plugins/ShrinkUrl/plugin.py
@@ -238,7 +238,6 @@ class ShrinkUrl(callbacks.PluginRegexp):
 
     _gooApi = 'https://www.googleapis.com/urlshortener/v1/url'
     def _getGooUrl(self, url):
-        url = utils.web.urlquote(url)
         try:
             return self.db.get('goo', url)
         except KeyError:
