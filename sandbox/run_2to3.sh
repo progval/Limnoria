cca03ba06b481b48a162ac22f016b9540fbcc662
Valentin Lorentz <progval@progval.net>
2013-01-06 13:39:56 +0100

    Copy docs to py3k/ too.

--- sandbox/run_2to3.sh
+++ sandbox/run_2to3.sh
@@ -1,4 +1,5 @@
 rm -f src/version.py # Prevent 2to3 from copying it, since py3k/src/version.py was probably written by root.
 cp locale/ py3k/ -R
+cp docs/ py3k/ -R
 cp plugins/ py3k/ -R # copy plugins data
 python 2to3/run.py src/ plugins/ test/ scripts/* setup.py -wWno py3k -f all -f def_iteritems -f def_itervalues -f def_iterkeys -f reload "$@"
