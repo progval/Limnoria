f42ac86d91802a2a8a37a15268d2b8cf898dd55d
Valentin Lorentz <progval@progval.net>
2013-01-30 20:10:48 +0100

    Make the setup.py runnable by Python 3 and run 2to3 automatically.

--- sandbox/run_2to3.sh
+++
@@ -1,7 +0,0 @@
-rm -f src/version.py # Prevent 2to3 from copying it, since py3k/src/version.py was probably written by root.
-rm -rf py3k
-mkdir py3k
-cp -R locales/ py3k/locales
-cp -R docs/ py3k/
-cp -R plugins/ py3k/plugins # copy plugins data
-python 2to3/run.py src/ plugins/ test/ scripts/* setup.py -wWno py3k -f all -f def_iteritems -f def_itervalues -f def_iterkeys -f reload "$@"
