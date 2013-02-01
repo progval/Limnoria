33048545eacee0edadc821f06833a60d7844c538
Valentin Lorentz <progval@gmail.com>
2013-02-01 14:49:53 +0000

    Revert "Google: Remove translate command since their API is gone"
    This reverts commit 304facd1672ee7f359f40e46b88f2bccfb1de65b.
    
    Conflicts:
    	plugins/Google/test.py

--- plugins/Google/test.py
+++ plugins/Google/test.py
@@ -54,4 +54,10 @@ class GoogleTestCase(ChannelPluginTestCase):
             self.assertRegexp('fight supybot moobot', r'.*supybot.*: \d+')
             self.assertNotError('fight ... !')
 
+        def testTranslate(self):
+            self.assertRegexp('translate en es hello world', 'mundo')
+
+        def testCalcDoesNotHaveExtraSpaces(self):
+            self.assertNotRegexp('google calc 1000^2', r'\s+,\s+')
+
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
