f1198fb4e8eb6105cb613d2c8529e16db8ae1268
nyuszika7h <nyuszika7h@cadoth.net>
2013-02-03 15:08:36 +0100

    irclib.py: Fix SASL on py3k

--- src/irclib.py
+++ src/irclib.py
@@ -906,7 +906,8 @@ class Irc(IrcCommandDispatcher):
                 if not self.sasl_username:
                     log.error('SASL username is not set, unable to identify.')
                 else:
-                    auth_string = base64.b64encode('%s\x00%s\x00%s' % (self.sasl_username,
+                    auth_string =
+                    base64.b64encode('%s\x00%s\x00%s'.encode('utf-8') % (self.sasl_username,
                         self.sasl_username, self.sasl_password))
                     log.debug('Sending CAP REQ command, requesting capability \'sasl\'.')
                     self.queueMsg(ircmsgs.IrcMsg(command="CAP", args=('REQ', 'sasl')))
