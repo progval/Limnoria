ee1d8731c94b052aafbbff927a9ea858514b1bc7
Valentin Lorentz <progval@progval.net>
2013-05-10 22:08:59 +0200

    sys.modules is a dict, not a list.

--- src/plugin.py
+++ src/plugin.py
@@ -60,7 +60,8 @@ def loadPluginModule(name, ignoreDeprecation=False):
         module = imp.load_module(name, *moduleInfo)
     except:
         sys.modules.pop(name, None)
-        sys.modules = filter(lambda x:not x.startswith(name + '.'), sys.modules)
+        sys.modules = filter(lambda x:not x[0].startswith(name + '.'),
+            sys.modules.items())
         raise
     if 'deprecated' in module.__dict__ and module.deprecated:
         if ignoreDeprecation:
