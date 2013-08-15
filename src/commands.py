progval/Limnoria

commit 29dc1438a0bb167462e3c750c9bfe4d0c463bb23

commands.py: Fix error message of getSomethingWithoutSpaces.

src/commands.py
@@ -578,7 +578,8 @@ def getSomething(irc, msg, args, state, errorMsg=None, p=None):
 def getSomethingNoSpaces(irc, msg, args, state, *L):
     def p(s):
         return len(s.split(None, 1)) == 1
-    getSomething(irc, msg, args, state, p=p, *L)
+    getSomething(irc, msg, args, state, p=p, errorMsg=_('You must not give a '
+            'string containing spaces as an argument.'), *L)
 
 def private(irc, msg, args, state):
     if irc.isChannel(msg.args[0]):

commit b375e7dfb298e958ba24233f2a0336daf4841d82

commands.py: Fix crash if a custom error message is provided to somethingWithoutSpaces (bug introduced in 29dc1438a0).

src/commands.py
@@ -600,8 +600,8 @@ def getSomething(irc, msg, args, state, errorMsg=None, p=None):
 def getSomethingNoSpaces(irc, msg, args, state, *L):
     def p(s):
         return len(s.split(None, 1)) == 1
-    getSomething(irc, msg, args, state, p=p, errorMsg=_('You must not give a '
-            'string containing spaces as an argument.'), *L)
+    L = L or [_('You must not give a string containing spaces as an argument.')]
+    getSomething(irc, msg, args, state, p=p, *L)
 
 def private(irc, msg, args, state):
     if irc.isChannel(msg.args[0]):