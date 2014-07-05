a1a6257a2d5cb66f7c5fc1645510021860ea7ac5
Mikaela Suomalainen <mikaela.suomalainen@outlook.com>
2014-07-05 10:58:43 +0300

    .travis.runonce.bash: remove broken if
    ```
    +echo '(detached from 8d70cf1) is not master nor testing, doing
    nothing.'
    (detached from 8d70cf1) is not master nor testing, doing nothing.
    ```

--- .travis.runonce.bash
+++ .travis.runonce.bash
@@ -25,11 +25,5 @@ cd docs
 sphinx-build -n -b html -d _build/doctrees . _build/html
 cd ..
 
-# Do these things only on testing or master.
-if [[ "$branch" = "master" || "$branch" = "testing" ]]; then
-    # Notify read the docs
-    curl -X POST http://readthedocs.org/build/limnoria
-    # Add other things which we want to do here, before the fi.
-else
-    echo "$branch is not master nor testing, doing nothing."
-fi
+# Notify read the docs
+curl -X POST http://readthedocs.org/build/limnoria
