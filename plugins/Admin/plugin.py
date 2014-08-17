89410ba0435f11eaf11fd49cdcbc4157f3527b0b
nyuszika7h <nyuszika7h@gmail.com>
2014-08-17 13:16:45 +0200

    Revert "Admin: Clarify error on 435 numeric"
    This reverts commit cf08ba2f02349b54f3194907a611f30a3ebbec68.

--- plugins/Admin/plugin.py
+++ plugins/Admin/plugin.py
@@ -192,8 +192,7 @@ class Admin(callbacks.Plugin):
     def do435(self, irc, msg):
         irc = self.pendingNickChanges.get(irc, None)
         if irc is not None:
-            irc.error(_('I can\'t change nick, I\'m currently banned in %s.') %
-                      msg.args[2])
+            irc.error(_('That nick is currently banned.'))
         else:
             self.log.debug('Got 435 without Admin.nick being called.')
 
