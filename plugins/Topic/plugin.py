0d4f7c655df755b636368c90d7dcb6f8875a9a41
GLolol <GLolol1@hotmail.com>
2014-09-19 12:22:12 -0700

    Topic: actually read 'requireManageCapability' per channel

--- plugins/Topic/plugin.py
+++ plugins/Topic/plugin.py
@@ -228,7 +228,7 @@ class Topic(callbacks.Plugin):
         c = irc.state.channels[channel]
         if msg.nick in c.ops or msg.nick in c.halfops or 't' not in c.modes:
             return True
-        capabilities = self.registryValue('requireManageCapability')
+        capabilities = self.registryValue('requireManageCapability', channel)
         if capabilities:
             for capability in re.split(r'\s*;\s*', capabilities):
                 if capability.startswith('channel,'):
