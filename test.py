f627837496cc5c4cce89968e9b28802b92512aa4
James Lu <james@overdrivenetworks.com>
2014-12-11 18:39:59 -0800

    DDG/PkgInfo: Update tests
    From: https://github.com/jlu5/SupyPlugins/commit/f73028a0ed9ba5c4261069294347b8c0e7f9fe41

--- test.py
+++ test.py
@@ -33,5 +33,8 @@ from supybot.test import *
 class DDGTestCase(PluginTestCase):
     plugins = ('DDG',)
 
+    def testSearch(self):
+        self.assertRegexp('ddg search wikipedia', 'Wikipedia.*? - .*?https?\:\/\/')
+
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
