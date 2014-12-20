d3c77bbc726f7115a9febfea95e1882921212787
James Lu <james@overdrivenetworks.com>
2014-12-19 22:44:45 -0800

    DDG: fill in __init__.py
    From: https://github.com/jlu5/SupyPlugins/commit/e9d916765015f6e3993f0708e53ae2fdd729a89d

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@
 DDG: Searches for results on DuckDuckGo.
 """
 
-import supybot
+iimport supybot
 import supybot.world as world
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
@@ -40,14 +40,15 @@ import supybot.world as world
 __version__ = ""
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.authors.unknown
+__author__ = supybot.Author('James Lu', 'GLolol',
+                            'GLolol@overdrive.pw')
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = ''
+__url__ = 'https://github.com/GLolol/SupyPlugins/'
 
 from . import config
 from . import plugin
