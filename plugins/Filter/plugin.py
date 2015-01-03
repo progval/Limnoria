progval/Limnoria

commit b78319ba2aa4373e95768dc1067d985be16bb73e

Filter: remove leading space from !spellit output

plugins/Filter/plugin.py
@@ -642,7 +642,7 @@ class Filter(callbacks.Plugin):
             except KeyError:
                 pass
             write(c)
-        irc.reply(out.getvalue())
+        irc.reply(out.getvalue()[1:])
     spellit = wrap(spellit, ['text'])
 
     @internationalizeDocstring

commit f2c66480bc133c97b65acf334c8775994658feb2

Filter: fix a bug introduced by b78319ba2aa
This caused the first character in text to be cut off if it is not set to be replaced (e.g. a letter
when 'config plugins.format.spellit.replaceletters' is off).

plugins/Filter/plugin.py
@@ -619,7 +619,7 @@ class Filter(callbacks.Plugin):
             except KeyError:
                 pass
             write(c)
-        irc.reply(out.getvalue()[1:])
+        irc.reply(out.getvalue().strip())
     spellit = wrap(spellit, ['text'])
 
     @internationalizeDocstring