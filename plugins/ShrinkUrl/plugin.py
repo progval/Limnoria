1d722f9c307234f19e2d9be7d815b2b7e7a0e6ad
nyuszika7h <nyuszika7h@openmailbox.org>
2015-01-24 19:33:33 +0100

    ShrinkUrl: Remove xrl.us
    > Please note: Adding new links has been disabled since September 2014
    > after 14 months notice.
    
    Source: http://metamark.net/

--- plugins/ShrinkUrl/plugin.py
+++ plugins/ShrinkUrl/plugin.py
@@ -229,35 +229,6 @@ class ShrinkUrl(callbacks.PluginRegexp):
             irc.errorPossibleBug(str(e))
     tiny = thread(wrap(tiny, ['url']))
 
-    _xrlApi = 'http://metamark.net/api/rest/simple'
-    @retry
-    def _getXrlUrl(self, url):
-        quotedurl = utils.web.urlquote(url)
-        try:
-            return self.db.get('xrl', quotedurl)
-        except KeyError:
-            data = utils.web.urlencode({'long_url': url})
-            text = utils.web.getUrl(self._xrlApi, data=data).decode()
-            if text.startswith('ERROR:'):
-                raise ShrinkError(text[6:])
-            self.db.set('xrl', quotedurl, text)
-            return text
-
-    @internationalizeDocstring
-    def xrl(self, irc, msg, args, url):
-        """<url>
-
-        Returns an xrl.us version of <url>.
-        """
-        try:
-            xrlurl = self._getXrlUrl(url)
-            m = irc.reply(xrlurl)
-            if m is not None:
-                m.tag('shrunken')
-        except ShrinkError as e:
-            irc.error(str(e))
-    xrl = thread(wrap(xrl, ['url']))
-
     _gooApi = 'https://www.googleapis.com/urlshortener/v1/url'
     @retry
     def _getGooUrl(self, url):
