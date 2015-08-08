5b9e07ecd28c4a24767f7a2997155ba647858157
Valentin Lorentz <progval@progval.net>
2015-08-09 00:47:50 +0200

    Fix previous commit.

--- plugins/Owner/plugin.py
+++ plugins/Owner/plugin.py
@@ -47,7 +47,6 @@ from supybot.commands import *
 import supybot.irclib as irclib
 import supybot.plugin as plugin
 import supybot.plugins as plugins
-import supybot.minisix as minisix
 import supybot.drivers as drivers
 import supybot.ircmsgs as ircmsgs
 import supybot.ircutils as ircutils
@@ -399,7 +398,7 @@ class Owner(callbacks.Plugin):
             L.append(format('linecache line cache flushed: %n cleared.',
                             (len(linecache.cache), 'line')))
             linecache.clearcache()
-            if minisix.PY2:
+            if sys.version_info[0] >= 3:
                 sys.exc_clear()
         collected = world.upkeep()
         if gc.garbage:
