8cf6914abb9bc3c787a296489fdf0e6395307223
Ken Spencer <iota@electrocode.net>
2016-11-26 21:48:50 -0500

    User: allow for custom whoami error

--- plugins/User/plugin.py
+++ plugins/User/plugin.py
@@ -480,7 +480,10 @@ class User(callbacks.Plugin):
             user = ircdb.users.getUser(msg.prefix)
             irc.reply(user.name)
         except KeyError:
-            irc.reply(_('I don\'t recognize you. You can message me either of these two commands: "user identify <username> <password>" to log in or "user register <username> <password>" to register.'))
+            if not self.registryValue('customWho'):
+                irc.reply(_('I don\'t recognize you. You can message me either of these two commands: "user identify <username> <password>" to log in or "user register <username> <password>" to register.'))
+            else:
+                irc.reply(self.registryValue('customWho'))
     whoami = wrap(whoami)
 
     @internationalizeDocstring
