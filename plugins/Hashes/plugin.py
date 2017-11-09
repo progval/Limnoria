b84ce3e43f63a917bbb9a2c47d3ac3b4c7d4a413
Ken Spencer <iota@electrocode.net>
2017-11-09 11:56:43 -0500

    Add hashes plugin (#1315)
    * add plugin Hashes, containing hash and encryption related commands
    
    * Hashes: make the default plugin Hashes when the commands are used by themselves
    
    * Hashes: add supybot.conf because of registerDefaultPlugin
    
    * Hashes: import registry to help with registerDefaultPlugin
    
    * Hashes: add in nicks for test
    
    * actually use hashlib instead of crypt, also s/'String'/'Hashes'
    
    * Hashes/test: add group in for testing
    
    * Hashes: add in changes per @GLolol
    
    * Hashes: remove xor
    
    * Hashes: make plugins = ('Hashes',) instead of ('Hashes') because python is weird
    
    * Hashes: fix sha512 calling sha256, use utils.str.format on 'algorithms'
    
    * Hashes: just use the variable instead of substitution per @GLolol
    
    * Hashes: follow through with not using substitution
    
    * Hashes: disable the 'algorithms' and 'mkhash' commands if the underlying 'hashlib.algorithms_available' isn't available
    
    * Hashes: don't use registerDefaultPlugin its for core plugins
    
    * Hashes: also somehow I didn't change the copyright
    
    * Hashes/test: fix copyright in file

---
+++ plugins/Hashes/plugin.py
@@ -0,0 +1,115 @@
+###
+# Copyright (c) 2017, Ken Spencer
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+###
+
+import hashlib
+
+import supybot.conf as conf
+import supybot.registry as registry
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.commands as commands
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+from supybot.i18n import PluginInternationalization, internationalizeDocstring
+_ = PluginInternationalization('Hashes')
+
+class Hashes(callbacks.Plugin):
+    """Provides hash or encryption related commands"""
+
+    @internationalizeDocstring
+    def md5(self, irc, msg, args, text):
+        """<text>
+
+        Returns the md5 hash of a given string.  Read
+        http://www.rsasecurity.com/rsalabs/faq/3-6-6.html for more information
+        about md5.
+        """
+        irc.reply(hashlib.md5(text.encode('utf8')).hexdigest())
+    md5 = wrap(md5, ['text'])
+
+    @internationalizeDocstring
+    def sha(self, irc, msg, args, text):
+        """<text>
+
+        Returns the SHA hash of a given string.  Read
+        http://www.secure-hash-algorithm-md5-sha-1.co.uk/ for more information
+        about SHA.
+        """
+        irc.reply(hashlib.sha1(text.encode('utf8')).hexdigest())
+    sha = wrap(sha, ['text'])
+
+    @internationalizeDocstring
+    def sha256(self, irc, msg, args, text):
+        """<text>
+
+        Returns a SHA256 hash of the given string.
+        """
+        irc.reply(hashlib.sha256(text.encode('utf8')).hexdigest())
+    sha256 = wrap(sha256, ['text'])
+
+    @internationalizeDocstring
+    def sha512(self, irc, msg, args, text):
+        """<text>
+
+        Returns a SHA512 hash of the given string.
+        """
+        irc.reply(hashlib.sha512(text.encode('utf8')).hexdigest())
+    sha512 = wrap(sha512, ['text'])
+
+    @internationalizeDocstring
+    def algorithms(self, irc, msg, args):
+        """<takes no arguments>
+
+        Returns the list of available algorithms."""
+        try:
+            irc.reply(utils.str.format("%L", hashlib.algorithms_available))
+        except AttributeError:
+            pass # allow building but not using, usually python <2.7
+    if hasattr(hashlib, 'algorithms_available'):
+        algorithms = wrap(algorithms)
+
+    @internationalizeDocstring
+    def mkhash(self, irc, msg, args, algorithm, text):
+        """<algorithm> <text>
+
+        Returns TEXT after it has been hashed with ALGORITHM. See the 'algorithms' command in this plugin to return the algorithms available on this system."""
+        algos = []
+        try:
+            algos = hashlib.algorithms_available
+        except:
+            pass # allow building but not using, usually python <2.7
+        if algorithm not in algos:
+            irc.error("Algorithm not available.")
+        else:
+            irc.reply(hashlib.new(algorithm, text.encode('utf8')).hexdigest())
+    if hasattr(hashlib, 'algorithms_available'):
+        mkhash = wrap(mkhash, ['something', 'text'])
+
+Class = Hashes
---
+++ plugins/Hashes/test.py
@@ -0,0 +1,68 @@
+###
+# Copyright (c) 2017, Ken Spencer
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+###
+
+import re
+
+from supybot.test import *
+import supybot.utils as utils
+
+try:
+    from unittest import skipIf
+except ImportError: # Python 2.6
+    def skipIf(cond, reason):
+        if cond:
+            print('Skipped: %s' % reason)
+            def decorator(f):
+                return None
+        else:
+            def decorator(f):
+                return f
+        return decorator
+
+try:
+    from hashlib import algorithms_available
+    has_algos = True
+except ImportError:
+    has_algos = False
+
+brown_fox = 'The quick brown fox jumped over the lazy dog'
+
+class HashesTestCase(PluginTestCase):
+    plugins = ('Hashes',)
+
+    def testHashes(self):
+        self.assertResponse('md5 %s' % brown_fox, '08a008a01d498c404b0c30852b39d3b8')
+        self.assertResponse('sha %s' % brown_fox, 'f6513640f3045e9768b239785625caa6a2588842')
+        self.assertResponse('sha256 %s' % brown_fox, '7d38b5cd25a2baf85ad3bb5b9311383e671a8a142eb302b324d4a5fba8748c69')
+        self.assertResponse('sha512 %s' % brown_fox, 'db25330cfa5d14eaadf11a6263371cfa0e70fcd7a63a433b91f2300ca25d45b66a7b50d2f6747995c8fa0ff365b28974792e7acd5624e1ddd0d66731f346f0e7')
+    @skipIf(not has_algos, "'hashlib.algorithms_available' is not available")
+    def testHashNew(self):
+        self.assertResponse('mkhash md5 %s' % brown_fox, '08a008a01d498c404b0c30852b39d3b8')
+        self.assertError('mkhash NonExistant %s' % brown_fox)
+        self.assertNotError('algorithms')
