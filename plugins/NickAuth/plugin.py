progval/Limnoria

commit e7329316880c486769d260a0d0d05a9d450ea5c1

Modified NickAuth to match the new WHOX

plugins/NickAuth/plugin.py
@@ -199,10 +199,11 @@ class NickAuth(callbacks.Plugin):
             self._auth(irc, msg.prefix, account)
 
     def do354(self, irc, msg):
-        if len(msg.args) != 6 or msg.args[1] != '1':
+        if len(msg.args) != 9 or msg.args[1] != '1':
             return
 
-        (__, ___, ident, host, nick, account) = msg.args
+        # irc.nick 1 user ip host nick status account gecos
+        (n, t, user, ip, host, nick, status, account, gecos) = msg.args
         prefix = '%s!%s@%s' % (nick, ident, host)
         user = ircdb.users.getUserFromNick(irc.network, account)
 

commit 5daaa14825f33fef59f88a2af3cd94211df45a22

NickAuth: Fix NameError caused by e7329316880c486769d260a0d0d05a9d450ea5c1.

plugins/NickAuth/plugin.py
@@ -203,7 +203,7 @@ class NickAuth(callbacks.Plugin):
             return
 
         # irc.nick 1 user ip host nick status account gecos
-        (n, t, user, ip, host, nick, status, account, gecos) = msg.args
+        (n, t, ident, ip, host, nick, status, account, gecos) = msg.args
         prefix = '%s!%s@%s' % (nick, ident, host)
         user = ircdb.users.getUserFromNick(irc.network, account)
 