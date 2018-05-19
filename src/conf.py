43ee68ee3da973e301e1a2e5ce3623c2da47272c
James Lu <james@overdrivenetworks.com>
2018-05-19 12:45:58 -0700

    conf: maxHistoryLength no longer requires a restart to apply
    03c4529d9e3f6ce13c288fa1aa4bfb933288f27d made irc.state.history network specific, so reconnecting each network separately will update its history buffer's max length.

--- src/conf.py
+++ src/conf.py
@@ -1158,7 +1158,7 @@ registerGlobalValue(supybot.protocols.irc, 'vhostv6',
 registerGlobalValue(supybot.protocols.irc, 'maxHistoryLength',
     registry.Integer(1000, _("""Determines how many old messages the bot will
     keep around in its history.  Changing this variable will not take effect
-    until the bot is restarted.""")))
+    on a network until it is reconnected.""")))
 
 registerGlobalValue(supybot.protocols.irc, 'throttleTime',
     registry.Float(1.0, _("""A floating point number of seconds to throttle
