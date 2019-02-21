progval/Limnoria

commit b1db15c0562ff9f719570842358b7e6d2605a209

Add a symlink supybot pointing to src/ + move locales/ to src/
This looks like a more standard source tree, and allows for running tests without installing.

setup.py
@@ -147,19 +147,9 @@ packages = ['supybot',
              'supybot.plugins.Math.local',
             ]
 
-package_dir = {'supybot': 'src',
-               'supybot.utils': 'src/utils',
-               'supybot.locales': 'locales',
-               'supybot.plugins': 'plugins',
-               'supybot.drivers': 'src/drivers',
-               'supybot.plugins.Dict.local': 'plugins/Dict/local',
-               'supybot.plugins.Math.local': 'plugins/Math/local',
-              }
-
-package_data = {'supybot.locales': [s for s in os.listdir('locales/')]}
+package_data = {'supybot.locales': [s for s in os.listdir('supybot/locales/')]}
 
 for plugin in plugins:
-    package_dir['supybot.plugins.' + plugin] = 'plugins/' + plugin
     locales_path = 'plugins/' + plugin + '/locales/'
     locales_name = 'supybot.plugins.'+plugin
     if os.path.exists(locales_path):
@@ -209,8 +199,6 @@ setup(
 
     # Installation data
     packages=packages,
-
-    package_dir=package_dir,
 
     package_data=package_data,
 

.gitignore
@@ -23,7 +23,6 @@ supybot.egg-info/
 test-conf/
 test-data/
 test-logs/
-src/version.py
 INSTALL
 README.txt
 conf/

Makefile
@@ -1,4 +1,4 @@
-PYTHON=`which python`
+PYTHON=`which python3`
 DESTDIR=/
 PROJECT=limnoria
 
@@ -9,6 +9,9 @@ all:
 	@echo "make builddeb_py2 - Generate a deb package for Python 2"
 	@echo "make builddeb_py3 - Generate a deb package for Python 3"
 	@echo "make clean - Get rid of scratch and byte files"
+
+test:
+	PATH=./scripts/:${PATH} PYTHONPATH=. $(PYTHON) ./scripts/supybot-test test --plugins-dir=plugins/
 
 source:
 	$(PYTHON) setup.py sdist $(COMPILE)
@@ -35,4 +38,5 @@ clean:
 	rm -rf build/ MANIFEST
 	find . -name '*.pyc' -delete
 	rm debian/control
-	
+
+.PHONY: test

commit b882d1cc00924e2cd7b9cb6769e7c637a7fdd1f2

Fix PyPI packages.
It was broken by b1db15c0562ff9f719570842358b7e6d2605a209.

setup.py
@@ -48,7 +48,10 @@ if debug:
 if path:
     os.chdir(path)
 
-VERSION_FILE = os.path.join('src', 'version.py')
+if os.path.isdir('src'):
+    VERSION_FILE = os.path.join('src', 'version.py')
+else:
+    VERSION_FILE = os.path.join('supybot', 'version.py')
 version = None
 try:
     if 'SOURCE_DATE_EPOCH' in os.environ:
@@ -64,7 +67,7 @@ try:
             time.strptime(time.asctime(time.gmtime(date)))[:3])
 except:
     if os.path.isfile(VERSION_FILE):
-        from src.version import version
+        from supybot.version import version
     else:
         version = 'installed on ' + time.strftime("%Y-%m-%dT%H-%M-%S", time.gmtime())
 try:
@@ -72,7 +75,7 @@ try:
 except OSError: # Does not exist
     pass
 if version:
-    fd = open(os.path.join('src', 'version.py'), 'a')
+    fd = open(VERSION_FILE, 'a')
     fd.write("version = '%s'\n" % version)
     fd.write('try: # For import from setup.py\n')
     fd.write('    import supybot.utils.python\n')