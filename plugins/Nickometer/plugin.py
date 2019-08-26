progval/Limnoria

commit 88c2c130cac98e478bc1966003f38e213e032e45

BadWords & Nickometer: Use 'future' divisions.

plugins/Nickometer/plugin.py
@@ -45,6 +45,8 @@
 #    #
 ###
 
+from __future__ import division
+
 import supybot
 
 import re
@@ -220,7 +222,7 @@ class Nickometer(callbacks.Plugin):
 
         # Use an appropriate function to map [0, +inf) to [0, 100)
         percentage = 100 * (1 + math.tanh((score - 400.0) / 400.0)) * \
-                     (1 - 1 / (1 + score / 5.0)) / 2
+                     (1 - 1 / (1 + score / 5.0)) // 2
 
         # if it's above 99.9%, show as many digits as is interesting
         score_string=re.sub('(99\\.9*\\d|\\.\\d).*','\\1',`percentage`)

plugins/BadWords/config.py
@@ -27,6 +27,8 @@
 # POSSIBILITY OF SUCH DAMAGE.
 ###
 
+from __future__ import division
+
 import time
 
 import supybot.conf as conf
@@ -64,7 +66,7 @@ conf.registerChannelValue(BadWords,'requireWordBoundaries',
 class String256(registry.String):
     def __call__(self):
         s = registry.String.__call__(self)
-        return s * (1024/len(s))
+        return s * (1024//len(s))
 
     def __str__(self):
         return self.value

commit 0413304d5354cf72dd6477f52ac9e99a14b2532b

Nickometer: Use non-euclidian division as was originally intended.
I accidentally broke it in 88c2c130cac98e478bc1966003f38e213e032e45,
thinking the operands were both integers.

plugins/Nickometer/plugin.py
@@ -45,8 +45,6 @@
 #    #
 ###
 
-from __future__ import division
-
 import supybot
 
 import re
@@ -224,7 +222,7 @@ class Nickometer(callbacks.Plugin):
 
         # Use an appropriate function to map [0, +inf) to [0, 100)
         percentage = 100 * (1 + math.tanh((score - 400.0) / 400.0)) * \
-                     (1 - 1 / (1 + score / 5.0)) // 2
+                     (1 - 1 / (1 + score / 5.0)) / 2
 
         # if it's above 99.9%, show as many digits as is interesting
         score_string=re.sub('(99\\.9*\\d|\\.\\d).*','\\1',repr(percentage))

plugins/Nickometer/test.py
@@ -33,6 +33,12 @@ class NickometerTestCase(PluginTestCase):
     plugins = ('Nickometer',)
     def testNickometer(self):
         self.assertNotError('nickometer')
-        self.assertNotError('nickometer jemfinch')
+        self.assertResponse(
+            'nickometer jemfinch',
+            'The "lame nick-o-meter" reading for "jemfinch" is 0.0%.')
+        nick = 'xXReallyObnoxious1337NickXx'
+        self.assertResponse(
+            'nickometer %s' % nick,
+            'The "lame nick-o-meter" reading for "%s" is 99.96%%.' % nick)
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79: