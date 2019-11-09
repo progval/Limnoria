progval/Limnoria

commit 0014b206ada2cc1fbabe497a0048e2bfed608511

Reset capability set state on reconnection.

src/irclib.py
@@ -379,11 +379,15 @@ class IrcState(IrcCommandDispatcher, log.Firewalled):
     def reset(self):
         """Resets the state to normal, unconnected state."""
         self.history.reset()
+        self.history.resize(conf.supybot.protocols.irc.maxHistoryLength())
+        self.ircd = None
         self.channels.clear()
         self.supported.clear()
         self.nicksToHostmasks.clear()
-        self.history.resize(conf.supybot.protocols.irc.maxHistoryLength())
         self.batches = {}
+        self.capabilities_ack = set()
+        self.capabilities_nak = set()
+        self.capabilities_ls = set()
 
     def __reduce__(self):
         return (self.__class__, (self.history, self.supported,

commit 6a9462be008b797c8fb03f422606413c62ff21e8

Fix reset of capabilities_ls.
Broken by 0014b206ada2cc1fbabe497a0048e2bfed608511.

src/irclib.py
@@ -388,7 +388,7 @@ class IrcState(IrcCommandDispatcher, log.Firewalled):
         self.batches = {}
         self.capabilities_ack = set()
         self.capabilities_nak = set()
-        self.capabilities_ls = set()
+        self.capabilities_ls = {}
 
     def __reduce__(self):
         return (self.__class__, (self.history, self.supported,