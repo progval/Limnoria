progval/Limnoria

commit 04b55805b8175e0f29d0f4d77dbae2b91b96d5d1

Log SASL mechanism on failure. Closes GH-1014.

src/irclib.py
@@ -1225,7 +1225,8 @@ class Irc(IrcCommandDispatcher, log.Firewalled):
         self.endCapabilityNegociation()
 
     def do904(self, msg):
-        log.warning('%s: SASL authentication failed', self.network)
+        log.warning('%s: SASL authentication failed (mechanism: %s)',
+                self.network, self.sasl_next_mechanisms[0])
         self.tryNextSaslMechanism()
 
     def do905(self, msg):

commit 2dddfefa98b44b8d539455e73910cd689abb5218

Fix log on SASL failure.
Broken by 04b55805b8175e0f29d0f4d77dbae2b91b96d5d1.

src/irclib.py
@@ -1226,7 +1226,7 @@ class Irc(IrcCommandDispatcher, log.Firewalled):
 
     def do904(self, msg):
         log.warning('%s: SASL authentication failed (mechanism: %s)',
-                self.network, self.sasl_next_mechanisms[0])
+                self.network, self.sasl_current_mechanism)
         self.tryNextSaslMechanism()
 
     def do905(self, msg):