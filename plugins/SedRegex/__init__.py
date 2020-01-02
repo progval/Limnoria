jlu5/Limnoria-RelayNext

Update plugin author/maintainer data
From: https://github.com/jlu5/SupyPlugins/commit/cbd953b32c3da5f5d2bb5b9e711a467050a9dcb9

__init__.py
@@ -39,9 +39,8 @@ import supybot.world as world
 # in here if you're keeping the plugin in CVS or some similar system.
 __version__ = "2019.07.09+git"
 
-# XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.Author('James Lu', 'GLolol',
-                            'james@overdrivenetworks.com')
+__author__ = getattr(supybot.authors, 'jlu',
+                     supybot.Author('James Lu', 'GLolol', 'james@overdrivenetworks.com'))
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.

progval/Limnoria

plugins/SedRegex/__init__.py
@@ -43,6 +43,9 @@ __contributors__ = {supybot.Author("James Lu", "GLolol", "glolol@overdrivenetwor
                     supybot.Author('nyuszika7h', 'nyuszika7h', 'nyuszika7h@openmailbox.org'):
                     	["_unpack_sed method within plugin.py"]
                    }
+__maintainer__ = getattr(supybot.authors, 'jlu',
+                         supybot.Author('James Lu', 'GLolol', 'james@overdrivenetworks.com'))
+
 __url__ = 'https://github.com/jlu5/SupyPlugins'
 
 from . import config