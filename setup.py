16b7183c280d86c112156cd0c8151bd370a76f04
Valentin Lorentz <progval+git@progval.net>
2020-05-05 19:54:51 +0200

    Add python 3.8 classifier.

--- setup.py
+++ setup.py
@@ -210,6 +210,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Communications :: Chat :: Internet Relay Chat',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
