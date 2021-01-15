progval/Limnoria

commit c1ae3f5c81cc8cf4effc7670e99c9270e31a1c9c

all plugins: Use msg.channel instead of msg.args[0] + give network name to self.registryValue.

plugins/Relay/plugin.py
@@ -197,7 +197,7 @@ class Relay(callbacks.Plugin):
     do401 = do402
 
     def _formatPrivmsg(self, nick, network, msg):
-        channel = msg.args[0]
+        channel = msg.channel
         if self.registryValue('includeNetwork', channel):
             network = '@' + network
         else:
@@ -229,12 +229,12 @@ class Relay(callbacks.Plugin):
         assert msg.command in ('PRIVMSG', 'NOTICE', 'TOPIC')
         for otherIrc in world.ircs:
             if otherIrc != irc and not otherIrc.zombie:
-                if msg.args[0] in otherIrc.state.channels:
+                if msg.channel in otherIrc.state.channels:
                     msg.tag('relayedMsg')
                     otherIrc.queueMsg(msg)
 
     def _checkRelayMsg(self, msg):
-        channel = msg.args[0]
+        channel = msg.channel
         if channel in self.lastRelayMsgs:
             q = self.lastRelayMsgs[channel]
             unformatted = ircutils.stripFormatting(msg.args[1])
@@ -247,7 +247,7 @@ class Relay(callbacks.Plugin):
     def _punishRelayers(self, msg):
         assert self._checkRelayMsg(msg), 'Punishing without checking.'
         who = msg.prefix
-        channel = msg.args[0]
+        channel = msg.channel
         def notPunishing(irc, s, *args):
             self.log.info('Not punishing %s in %s on %s: %s.',
                           msg.prefix, channel, irc.network, s, *args)
@@ -268,12 +268,12 @@ class Relay(callbacks.Plugin):
     def doPrivmsg(self, irc, msg):
         if ircmsgs.isCtcp(msg) and not ircmsgs.isAction(msg):
             return
-        (channel, text) = msg.args
-        if irc.isChannel(channel):
+        text = msg.args[1]
+        if msg.channel:
             irc = self._getRealIrc(irc)
-            if channel not in self.registryValue('channels'):
+            if msg.channel not in self.registryValue('channels'):
                 return
-            ignores = self.registryValue('ignores', channel)
+            ignores = self.registryValue('ignores', msg.channel, irc.network)
             for ignore in ignores:
                 if ircutils.hostmaskPatternEqual(ignore, msg.prefix):
                     self.log.debug('Refusing to relay %s, ignored by %s.',
@@ -281,7 +281,8 @@ class Relay(callbacks.Plugin):
                     return
             # Let's try to detect other relay bots.
             if self._checkRelayMsg(msg):
-                if self.registryValue('punishOtherRelayBots', channel):
+                if self.registryValue('punishOtherRelayBots',
+                                      msg.channel, irc.network):
                     self._punishRelayers(msg)
                 # Either way, we don't relay the message.
                 else:
@@ -291,13 +292,12 @@ class Relay(callbacks.Plugin):
             else:
                 network = self._getIrcName(irc)
                 s = self._formatPrivmsg(msg.nick, network, msg)
-                m = self._msgmaker(channel, s)
+                m = self._msgmaker(msg.channel, network, s)
                 self._sendToOthers(irc, m)
 
-    def _msgmaker(self, target, s):
+    def _msgmaker(self, target, network, s):
         msg = dynamic.msg
-        channel = dynamic.channel
-        if self.registryValue('noticeNonPrivmsgs', dynamic.channel) and \
+        if self.registryValue('noticeNonPrivmsgs', target) and \
            msg.command != 'PRIVMSG':
             return ircmsgs.notice(target, s)
         else:
@@ -423,16 +423,15 @@ class Relay(callbacks.Plugin):
             if msg.relayedMsg:
                 self._addRelayMsg(msg)
             else:
-                channel = msg.args[0]
-                if channel in self.registryValue('channels'):
+                if msg.channel in self.registryValue('channels'):
                     network = self._getIrcName(irc)
                     s = self._formatPrivmsg(irc.nick, network, msg)
-                    relayMsg = self._msgmaker(channel, s)
+                    relayMsg = self._msgmaker(msg.args[0], s)
                     self._sendToOthers(irc, relayMsg)
         return msg
 
     def _addRelayMsg(self, msg):
-        channel = msg.args[0]
+        channel = msg.channel
         if channel in self.lastRelayMsgs:
             q = self.lastRelayMsgs[channel]
         else:

plugins/Alias/plugin.py
@@ -49,9 +49,9 @@ def getChannel(irc, msg, args):
     If the channel was given in args, args is modified (the channel is
     removed).
     """
-    if args and irc.isChannel(msg.args[0]):
+    if args and msg.channel:
         if conf.supybot.reply.requireChannelCommandsToBeSentInChannel():
-            if args[0] != msg.args[0]:
+            if args[0] != msg.channel:
                 s = 'Channel commands must be sent in the channel to which ' \
                     'they apply; if this is not the behavior you desire, ' \
                     'ask the bot\'s administrator to change the registry ' \
@@ -60,8 +60,8 @@ def getChannel(irc, msg, args):
                     'to False.'
                 raise callbacks.Error(s)
         return args.pop(0)
-    elif irc.isChannel(msg.args[0]):
-        return msg.args[0]
+    elif msg.channel:
+        return msg.channel
     else:
         raise callbacks.Error('Command must be sent in a channel or ' \
                                'include a channel in its arguments.')

plugins/Anonymous/plugin.py
@@ -50,17 +50,20 @@ class Anonymous(callbacks.Plugin):
     supybot.plugins.Anonymous.requireRegistration.
     """
     def _preCheck(self, irc, msg, target, action):
-        if self.registryValue('requireRegistration', target):
+        if self.registryValue('requireRegistration', target, irc.network):
             try:
                 foo = ircdb.users.getUser(msg.prefix)
             except KeyError:
                 irc.errorNotRegistered(Raise=True)
-        capability = self.registryValue('requireCapability', target)
+        capability = self.registryValue('requireCapability',
+                                        target, irc.network)
         if capability:
             if not ircdb.checkCapability(msg.prefix, capability):
                 irc.errorNoCapability(capability, Raise=True)
         if action != 'tell':
-            if self.registryValue('requirePresenceInChannel', target) and \
+            require_presence = self.registryValue('requirePresenceInChannel',
+                                                  target, irc.network)
+            if require_presence and \
                msg.nick not in irc.state.channels[target].users:
                 irc.error(format(_('You must be in %s to %q in there.'),
                                  target, action), Raise=True)

plugins/AutoMode/plugin.py
@@ -48,12 +48,13 @@ class AutoMode(callbacks.Plugin):
     """This plugin, when configured, allows the bot to automatically set modes
     on users when they join."""
     def doJoin(self, irc, msg):
-        channel = msg.args[0]
+        channel = msg.channel
+        network = irc.network
         if ircutils.strEqual(irc.nick, msg.nick):
             return
-        if not self.registryValue('enable', channel):
+        if not self.registryValue('enable', channel, network):
             return
-        fallthrough = self.registryValue('fallthrough', channel)
+        fallthrough = self.registryValue('fallthrough', channel, network)
         def do(type):
             cap = ircdb.makeChannelCapability(channel, type)
             cap_auto = ircdb.makeChannelCapability(channel, 'auto'+type)
@@ -63,7 +64,7 @@ class AutoMode(callbacks.Plugin):
                         ignoreChannelOp=True, ignoreDefaultAllow=True)
             except KeyError:
                 apply_mode = False
-            if self.registryValue('alternativeCapabilities', channel):
+            if self.registryValue('alternativeCapabilities', channel, network):
                 try:
                     override = ircdb.checkCapability(msg.prefix, cap_auto,
                             ignoreOwner=not self.registryValue('owner'),
@@ -73,9 +74,9 @@ class AutoMode(callbacks.Plugin):
             else:
                 override = False
             if apply_mode or override:
-                if override or self.registryValue(type, channel):
-                    self.log.info('Scheduling auto-%s of %s in %s.',
-                                  type, msg.prefix, channel)
+                if override or self.registryValue(type, channel, network):
+                    self.log.info('Scheduling auto-%s of %s in %s @ %s.',
+                                  type, msg.prefix, channel, network)
                     def dismiss():
                         """Determines whether or not a mode has already
                         been applied."""
@@ -87,8 +88,9 @@ class AutoMode(callbacks.Plugin):
                     raise Continue # Even if fallthrough, let's only do one.
                 elif not fallthrough:
                     self.log.debug('%s has %s, but supybot.plugins.AutoMode.%s'
-                                   ' is not enabled in %s, refusing to fall '
-                                   'through.', msg.prefix, cap, type, channel)
+                                   ' is not enabled in %s @ %s, refusing to '
+                                   'fall through.',
+                                   msg.prefix, cap, type, channel, network)
                     raise Continue
         def schedule_msg(msg, dismiss):
             def f():
@@ -96,7 +98,7 @@ class AutoMode(callbacks.Plugin):
                     irc.queueMsg(msg)
                 else:
                     self.log.info('Dismissing auto-mode for %s.', msg.args[2])
-            delay = self.registryValue('delay', channel)
+            delay = self.registryValue('delay', channel, network)
             if delay:
                 schedule.addEvent(f, time.time() + delay)
             else:
@@ -107,7 +109,7 @@ class AutoMode(callbacks.Plugin):
             except KeyError:
                 return
             pattern = re.compile('-|\+')
-            for item in self.registryValue('extra', channel):
+            for item in self.registryValue('extra', channel, network):
                 try:
                     username, modes = pattern.split(item, maxsplit=1)
                     modes = item[len(username)] + modes
@@ -118,8 +120,8 @@ class AutoMode(callbacks.Plugin):
                 if username != user.name:
                     continue
                 else:
-                    self.log.info('Scheduling auto-modes %s of %s in %s.',
-                                  modes, msg.prefix, channel)
+                    self.log.info('Scheduling auto-modes %s of %s in %s @ %s.',
+                                  modes, msg.prefix, channel, network)
                     modes = [modes] + \
                             ([msg.nick]*len(pattern.sub('', modes)))
                     schedule_msg(ircmsgs.mode(channel, modes), lambda :False)
@@ -133,8 +135,9 @@ class AutoMode(callbacks.Plugin):
         finally:
             extra_modes()
         c = ircdb.channels.getChannel(channel)
-        if c.checkBan(msg.prefix) and self.registryValue('ban', channel):
-            period = self.registryValue('ban.period', channel)
+        if c.checkBan(msg.prefix) and self.registryValue('ban',
+                                                         channel, network):
+            period = self.registryValue('ban.period', channel, network)
             if period:
                 def unban():
                     try:

plugins/BadWords/plugin.py
@@ -72,10 +72,11 @@ class BadWords(callbacks.Privmsg):
         # We need to check for bad words here rather than in doPrivmsg because
         # messages don't get to doPrivmsg if the user is ignored.
         if msg.command == 'PRIVMSG' and self.words():
-            channel = msg.args[0]
-            self.updateRegexp(channel)
+            channel = msg.channel
+            self.updateRegexp(channel, irc.network)
             s = ircutils.stripFormatting(msg.args[1])
-            if irc.isChannel(channel) and self.registryValue('kick', channel):
+            if irc.isChannel(channel) \
+                    and self.registryValue('kick', channel, irc.network):
                 if self.regexp.search(s):
                     c = irc.state.channels[channel]
                     cap = ircdb.makeChannelCapability(channel, 'op')
@@ -86,22 +87,23 @@ class BadWords(callbacks.Privmsg):
                                            "they are halfop+ or can't be "
                                            "kicked.", msg.nick, channel)
                         else:
-                            message = self.registryValue('kick.message', channel)
+                            message = self.registryValue('kick.message',
+                                                         channel, irc.network)
                             irc.queueMsg(ircmsgs.kick(channel, msg.nick, message))
                     else:
                         self.log.warning('Should kick %s from %s, but not opped.',
                                          msg.nick, channel)
         return msg
 
-    def updateRegexp(self, channel):
+    def updateRegexp(self, channel, network):
         if self.lastModified < self.words.lastModified:
-            self.makeRegexp(self.words(), channel)
+            self.makeRegexp(self.words(), channel, network)
             self.lastModified = time.time()
 
     def outFilter(self, irc, msg):
         if self.filtering and msg.command == 'PRIVMSG' and self.words():
-            channel = msg.args[0]
-            self.updateRegexp(channel)
+            channel = msg.channel
+            self.updateRegexp(channel, irc.network)
             s = msg.args[1]
             if self.registryValue('stripFormatting'):
                 s = ircutils.stripFormatting(s)
@@ -110,9 +112,9 @@ class BadWords(callbacks.Privmsg):
                 msg = ircmsgs.privmsg(msg.args[0], t, msg=msg)
         return msg
 
-    def makeRegexp(self, iterable, channel):
+    def makeRegexp(self, iterable, channel, network):
         s = '(%s)' % '|'.join(map(re.escape, iterable))
-        if self.registryValue('requireWordBoundaries', channel):
+        if self.registryValue('requireWordBoundaries', channel, network):
             s = r'\b%s\b' % s
         self.regexp = re.compile(s, re.I)
 

plugins/Channel/plugin.py
@@ -54,24 +54,26 @@ class Channel(callbacks.Plugin):
         self.invites = {}
 
     def doKick(self, irc, msg):
-        channel = msg.args[0]
+        channel = msg.channel
+        network = irc.network
         if msg.args[1] == irc.nick:
-            if self.registryValue('alwaysRejoin', channel):
-                delay = self.registryValue('rejoinDelay', channel)
+            if self.registryValue('alwaysRejoin', channel, network):
+                delay = self.registryValue('rejoinDelay', channel, network)
                 networkGroup = conf.supybot.networks.get(irc.network)
                 if delay:
                     def f():
                         irc.sendMsg(networkGroup.channels.join(channel))
                     schedule.addEvent(f, time.time() + delay)
-                    self.log.info('Kicked from %s by %s. Rejoining after %s '
-                                  'seconds.', channel, msg.prefix, delay)
+                    self.log.info('Kicked from %s @ %s by %s. '
+                                  'Rejoining after %s seconds.',
+                                  channel, network, msg.prefix, delay)
                 else:
-                    self.log.info('Kicked from %s by %s. Rejoining.',
-                                  channel, msg.prefix)
+                    self.log.info('Kicked from %s @ %s by %s. Rejoining.',
+                                  channel, network, msg.prefix)
                     irc.sendMsg(networkGroup.channels.join(channel))
             else:
-                self.log.info('Kicked from %s by %s. Not auto-rejoining.',
-                        channel, msg.prefix)
+                self.log.info('Kicked from %s @ %s by %s. Not auto-rejoining.',
+                        channel, network, msg.prefix)
 
     def _sendMsg(self, irc, msg):
         irc.queueMsg(msg)
@@ -274,7 +276,7 @@ class Channel(callbacks.Plugin):
         supybot.plugins.Channel.partMsg will be used. No part message will be
         used if neither a cycle reason nor a default part message is given.
         """
-        reason = (reason or self.registryValue("partMsg", channel))
+        reason = (reason or self.registryValue("partMsg", channel, irc.network))
         reason = ircutils.standardSubstitute(irc, msg, reason)
         self._sendMsg(irc, ircmsgs.part(channel, reason))
         networkGroup = conf.supybot.networks.get(irc.network)
@@ -907,12 +909,20 @@ class Channel(callbacks.Plugin):
         nicks if --count option is provided.
         """
         # Make sure we don't elicit information about private channels to
-        # people or channels that shouldn't know
+        # people or channels that shouldn't know. Someone is allowed if
+        # any of these are true:
+        # * the channel is not secret (mode +s),
+        # * the request is sent to the channel itself (FIXME: what about
+        #   channels without +n?),
+        # * the requester is op,
+        # * the request is not sent to a channel (or else everyone in the
+        #   channel would see the response) and the requester is in the
+        #   channel themselves
         capability = ircdb.makeChannelCapability(channel, 'op')
         if 's' in irc.state.channels[channel].modes and \
-            msg.args[0] != channel and \
+            msg.channel != channel and \
             not ircdb.checkCapability(msg.prefix, capability) and \
-            (irc.isChannel(msg.args[0]) or \
+            (msg.channel or \
              msg.nick not in irc.state.channels[channel].users):
             irc.error(_('You don\'t have access to that information.'),
                     Raise=True)
@@ -920,7 +930,7 @@ class Channel(callbacks.Plugin):
         keys = [option for (option, arg) in optlist]
         if 'count' not in keys:
             utils.sortBy(str.lower, L)
-            private = self.registryValue("nicksInPrivate", channel)
+            private = self.registryValue("nicksInPrivate", channel, irc.network)
             irc.reply(utils.str.commaAndify(L), private=private)
         else:
             irc.reply(str(len(L)))
@@ -961,11 +971,9 @@ class Channel(callbacks.Plugin):
         supybot.plugins.Channel.partMsg will be used. No part message will be
         used if no default is configured.
         """
-        if channel is None:
-            if irc.isChannel(msg.args[0]):
-                channel = msg.args[0]
-            else:
-                irc.error(Raise=True)
+        channel = channel or msg.channel
+        if not channel:
+            irc.error(Raise=True)
         capability = ircdb.makeChannelCapability(channel, 'op')
         if not ircdb.checkCapabilities(msg.prefix, [capability, 'admin']):
             irc.errorNoCapability(capability, Raise=True)
@@ -976,7 +984,7 @@ class Channel(callbacks.Plugin):
             pass
         if channel not in irc.state.channels:
             irc.error(_('I\'m not in %s.') % channel, Raise=True)
-        reason = (reason or self.registryValue("partMsg", channel))
+        reason = (reason or self.registryValue("partMsg", channel, irc.network))
         reason = ircutils.standardSubstitute(irc, msg, reason)
         irc.queueMsg(ircmsgs.part(channel, reason))
         if msg.nick in irc.state.channels[channel].users:

plugins/ChannelLogger/plugin.py
@@ -90,11 +90,11 @@ class ChannelLogger(callbacks.Plugin):
                     self.log.exception('Odd exception:')
 
     def logNameTimestamp(self, channel):
-        format = self.registryValue('filenameTimestamp', channel)
+        format = self.registryValue('filenameTimestamp', channel, irc.network)
         return time.strftime(format)
 
     def getLogName(self, channel):
-        if self.registryValue('rotateLogs', channel):
+        if self.registryValue('rotateLogs', channel, irc.network):
             return '%s.%s.log' % (channel, self.logNameTimestamp(channel))
         else:
             return '%s.log' % channel
@@ -118,7 +118,7 @@ class ChannelLogger(callbacks.Plugin):
     def checkLogNames(self):
         for (irc, logs) in self.logs.items():
             for (channel, log) in list(logs.items()):
-                if self.registryValue('rotateLogs', channel):
+                if self.registryValue('rotateLogs', channel, irc.network):
                     name = self.getLogName(channel)
                     if name != os.path.basename(log.name):
                         log.close()
@@ -156,26 +156,27 @@ class ChannelLogger(callbacks.Plugin):
         return ircutils.toLower(channel)
 
     def doLog(self, irc, channel, s, *args):
-        if not self.registryValue('enable', channel):
+        if not self.registryValue('enable', channel, irc.network):
             return
         s = format(s, *args)
         channel = self.normalizeChannel(irc, channel)
         log = self.getLog(irc, channel)
-        if self.registryValue('timestamp', channel):
+        if self.registryValue('timestamp', channel, irc.network):
             self.timestamp(log)
-        if self.registryValue('stripFormatting', channel):
+        if self.registryValue('stripFormatting', channel, irc.network):
             s = ircutils.stripFormatting(s)
         if minisix.PY2:
             s = s.decode('utf8', 'ignore')
         log.write(s)
-        if self.registryValue('flushImmediately'):
+        if self.registryValue('flushImmediately', irc.network):
             log.flush()
 
     def doPrivmsg(self, irc, msg):
         (recipients, text) = msg.args
         for channel in recipients.split(','):
             if irc.isChannel(channel):
-                noLogPrefix = self.registryValue('noLogPrefix', channel)
+                noLogPrefix = self.registryValue('noLogPrefix',
+                                                 channel, irc.network)
                 cap = ircdb.makeChannelCapability(channel, 'logChannelMessages')
                 try:
                     logChannelMessages = ircdb.checkCapability(msg.prefix, cap,
@@ -217,7 +218,7 @@ class ChannelLogger(callbacks.Plugin):
 
     def doJoin(self, irc, msg):
         for channel in msg.args[0].split(','):
-            if(self.registryValue('showJoinParts', channel)):
+            if(self.registryValue('showJoinParts', channel, irc.network)):
                 self.doLog(irc, channel,
                            '*** %s <%s> has joined %s\n',
                            msg.nick, msg.prefix, channel)
@@ -242,7 +243,7 @@ class ChannelLogger(callbacks.Plugin):
         else:
             reason = ""
         for channel in msg.args[0].split(','):
-            if(self.registryValue('showJoinParts', channel)):
+            if(self.registryValue('showJoinParts', channel, irc.network)):
                 self.doLog(irc, channel,
                            '*** %s <%s> has left %s%s\n',
                            msg.nick, msg.prefix, channel, reason)
@@ -268,7 +269,7 @@ class ChannelLogger(callbacks.Plugin):
         else:
             reason = ""
         for channel in msg.tagged('channels'):
-            if(self.registryValue('showJoinParts', channel)):
+            if(self.registryValue('showJoinParts', channel, irc.network)):
                 self.doLog(irc, channel,
                            '*** %s <%s> has quit IRC%s\n',
                            msg.nick, msg.prefix, reason)

plugins/ChannelStats/plugin.py
@@ -142,8 +142,8 @@ class StatsDB(plugins.ChannelUserDB):
             return UserStat(*L)
 
     def addMsg(self, irc, msg, id=None):
-        if msg.args and irc.isChannel(msg.args[0]):
-            channel = plugins.getChannel(msg.args[0])
+        if msg.channel:
+            channel = plugins.getChannel(msg.channel)
             if (channel, 'channelStats') not in self:
                 self[channel, 'channelStats'] = ChannelStat()
             self[channel, 'channelStats'].addMsg(msg)
@@ -186,8 +186,8 @@ class ChannelStats(callbacks.Plugin):
 
     def outFilter(self, irc, msg):
         if msg.command == 'PRIVMSG':
-            if irc.isChannel(msg.args[0]):
-                if self.registryValue('selfStats', msg.args[0]):
+            if msg.channel:
+                if self.registryValue('selfStats', msg.channel, irc.network):
                     try:
                         self.outFiltering = True
                         self.db.addMsg(irc, msg, 0)

commit 7110b8f74e59fceef7ea939ce019958085ff27f0

Relay: Set msg.channel before passing it to _sendToOthers.
_sendToOthers expects msg.channel to be 'in otherIrc.state.channels',
but IrcMsg objects don't have their 'channel' attribute set until they
are passed through irclib, so it was left unset, which means messages
were never sent at all.

Regression introduced in c1ae3f5c81cc8cf4effc7670e99c9270e31a1c9c.

plugins/Relay/plugin.py
@@ -299,9 +299,13 @@ class Relay(callbacks.Plugin):
         msg = dynamic.msg
         if self.registryValue('noticeNonPrivmsgs', target, network) and \
            msg.command != 'PRIVMSG':
-            return ircmsgs.notice(target, s)
+            m = ircmsgs.notice(target, s)
         else:
-            return ircmsgs.privmsg(target, s)
+            m = ircmsgs.privmsg(target, s)
+
+        m.channel = target
+
+        return m
 
     def doJoin(self, irc, msg):
         irc = self._getRealIrc(irc)