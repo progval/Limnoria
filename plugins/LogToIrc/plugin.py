e9ef8b22c084ac990f8a5ab2d0c611f7dd142dc3
Valentin Lorentz <progval+git@progval.net>
2021-03-10 22:35:35 +0100

    LogToIrc: Split into modules, like modern plugins

--- plugins/LogToIrc/plugin.py
+++ plugins/LogToIrc/plugin.py
@@ -33,13 +33,10 @@
 Allows for sending the bot's logging output to channels or users.
 """
 
-__revision__ = "$Id$"
-
 import supybot.plugins as plugins
 
 import logging
 import os.path
-from itertools import imap
 
 import supybot.log as log
 import supybot.conf as conf
@@ -137,81 +134,6 @@ _ircHandler = IrcHandler()
 _formatString = '%(name)s: %(levelname)s %(message)s'
 _ircFormatter = ColorizedIrcFormatter(_formatString)
 _ircHandler.setFormatter(_ircFormatter)
-
-class IrcLogLevel(log.ValidLogLevel):
-    """Value must be one of INFO, WARNING, ERROR, or CRITICAL."""
-    minimumLevel = logging.INFO
-    def setValue(self, v):
-        log.ValidLogLevel.setValue(self, v)
-        _ircHandler.setLevel(self.value)
-
-class ValidChannelOrNick(registry.String):
-    """Value must be a valid channel or a valid nick."""
-    def setValue(self, v):
-        if not (ircutils.isNick(v) or ircutils.isChannel(v)):
-            self.error()
-        registry.String.setValue(self, v)
-
-class Targets(registry.SpaceSeparatedListOfStrings):
-    Value = ValidChannelOrNick
-
-conf.registerPlugin('LogToIrc')
-conf.registerGlobalValue(conf.supybot.plugins.LogToIrc, 'level',
-    IrcLogLevel(logging.WARNING, """Determines what the minimum priority
-    level logged will be to IRC. See supybot.log.level for possible
-    values.  DEBUG is disabled due to the large quantity of output."""))
-conf.registerGlobalValue(conf.supybot.plugins.LogToIrc, 'targets',
-    Targets([], """Determines which channels/nicks the bot should
-    log to.  If no channels/nicks are set, this plugin will effectively be
-    turned off."""))
-conf.registerGlobalValue(conf.supybot.plugins.LogToIrc, 'networks',
-    registry.SpaceSeparatedSetOfStrings([], """Determines what networks the
-    bot should log to.  If no networks are set, the bot will log on one network
-    (whichever happens to be around at the time it feels like logging)."""))
-conf.registerGlobalValue(conf.supybot.plugins.LogToIrc, 'channelModesRequired',
-    registry.String('s', """Determines what channel modes a channel will be
-    required to have for the bot to log to the channel.  If this string is
-    empty, no modes will be checked."""))
-conf.registerGlobalValue(conf.supybot.plugins.LogToIrc,
-    'userCapabilityRequired', registry.String('owner', """Determines what
-    capability is required for the bot to log to in private messages to the
-    user.  If this is empty, there will be no capability that's checked."""))
-conf.registerGlobalValue(conf.supybot.plugins.LogToIrc, 'color',
-    registry.Boolean(False, """Determines whether the bot's logs to IRC will be
-    colorized with mIRC colors."""))
-conf.registerGlobalValue(conf.supybot.plugins.LogToIrc, 'notice',
-    registry.Boolean(False, """Determines whether the bot's logs to IRC will be
-    sent via NOTICE instead of PRIVMSG.  Channels will always be PRIVMSGed,
-    regardless of this variable; NOTICEs will only be used if this variable is
-    True and the target is a nick, not a channel."""))
-
-def configure(advanced):
-    from supybot.questions import something, anything, yn, output
-    output("""Here you can set which channels and who the bot has to send log
-              messages to. Note that by default in order to log to a channel
-              the channel has to have mode +s set. Logging to a user requires
-              the user to have the Owner capability.""")
-    targets = ''
-    while not targets:
-        try:
-            targets = anything('Which channels or users would you like to '
-                               'send log messages to?')
-            conf.supybot.plugins.LogToIrc.targets.set(targets)
-        except registry.InvalidRegistryValue, e:
-            output(str(e))
-            targets = ''
-    colorized = yn('Would you like these messages to be colored?')
-    conf.supybot.plugins.LogToIrc.color.setValue(colorized)
-    if advanced:
-        level = ''
-        while not level:
-            try:
-                level = something('What would you like the minimum priority '
-                                  'level to be which will be logged to IRC?')
-                conf.supybot.plugins.LogToIrc.level.set(level)
-            except registry.InvalidRegistryValue, e:
-                output(str(e))
-                level = ''
 
 
 class LogToIrc(callbacks.Privmsg):
