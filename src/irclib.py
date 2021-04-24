progval/Limnoria

commit 4d2efc65ef26f4c49d893a520c36830e600dad56

Remove some unused names

src/irclib.py
@@ -1564,7 +1564,6 @@ class Irc(IrcCommandDispatcher, log.Firewalled):
         received* tags."""
         if tag:
             self._tagMsg(msg)
-        channel = msg.channel
 
         preInFilter = str(msg).rstrip('\r\n')
         log.debug('Incoming message (%s): %s', self.network, preInFilter)

plugins/Admin/test.py
@@ -95,7 +95,6 @@ class AdminTestCase(PluginTestCase):
         self.assertEqual(m.args[1], 'key')
 
     def testNick(self):
-        original = conf.supybot.nick()
         try:
             m = self.getMsg('nick foobar')
             self.assertEqual(m.command, 'NICK')

plugins/Services/config.py
@@ -31,7 +31,7 @@
 import supybot.conf as conf
 import supybot.ircutils as ircutils
 import supybot.registry as registry
-from supybot.i18n import PluginInternationalization, internationalizeDocstring
+from supybot.i18n import PluginInternationalization
 _ = PluginInternationalization('Services')
 
 def registerNick(nick, password=''):
@@ -44,7 +44,7 @@ def registerNick(nick, password=''):
         v.setValue(password)
 
 def configure(advanced):
-    from supybot.questions import expect, anything, something, yn, getpass
+    from supybot.questions import something
     conf.registerPlugin('Services', True)
     nick = something(_('What is your registered nick?'))
     password = something(_('What is your password for that nick?'))

plugins/Services/plugin.py
@@ -38,7 +38,6 @@ import supybot.utils as utils
 from supybot.commands import *
 import supybot.ircmsgs as ircmsgs
 import supybot.ircutils as ircutils
-import supybot.schedule as schedule
 import supybot.callbacks as callbacks
 from supybot.i18n import PluginInternationalization, internationalizeDocstring
 _ = PluginInternationalization('Services')

commit 177c20267c703d9255b4fb24ef9ebf14773545ff

irclib: Fix crash caused by 4d2efc65ef26f4c49d893a520c36830e600dad56.
dynamicScope inspects the stack looking for this variable, so we need to keep it.

src/irclib.py
@@ -1564,6 +1564,7 @@ class Irc(IrcCommandDispatcher, log.Firewalled):
         received* tags."""
         if tag:
             self._tagMsg(msg)
+        channel = msg.channel  # used by dynamicScope (ew)
 
         preInFilter = str(msg).rstrip('\r\n')
         log.debug('Incoming message (%s): %s', self.network, preInFilter)