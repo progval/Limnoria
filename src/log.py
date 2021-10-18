progval/Limnoria

commit dff394df64b275833eba3b12f253f1e66c80db27

log: Add support for rotating messages.log.

src/log.py
@@ -34,6 +34,7 @@ import time
 import types
 import atexit
 import logging
+import logging.handlers
 import operator
 import textwrap
 import traceback
@@ -125,12 +126,14 @@ class StdoutStreamHandler(logging.StreamHandler):
             logging._releaseLock()
 
 
-class BetterFileHandler(logging.FileHandler):
+class BetterFileHandler(logging.handlers.WatchedFileHandler):
     def emit(self, record):
         try:
             try:
                 super().emit(record)
             except (UnicodeError, TypeError):
+                # the above line took care of calling reopenIfNeeded(), even
+                # if it raised one of these exceptions.
                 msg = self.format(record)
                 self.stream.write(repr(msg))
                 self.stream.write(os.linesep)

commit 1f618b9773a37443188b5ada17bee0306d05cf92

log: Fix support of 'supybot-test --clean'
Broken in dff394df64b275833eba3b12f253f1e66c80db27.

scripts/supybot-test
@@ -234,6 +234,7 @@ if __name__ == '__main__':
     print('Testing began at %s (pid %s)' % (time.ctime(), os.getpid()))
     if options.clean:
         shutil.rmtree(conf.supybot.directories.log())
+        log._mkDirs()
         shutil.rmtree(conf.supybot.directories.conf())
         shutil.rmtree(conf.supybot.directories.data())
     result = runner.run(suite)

src/log.py
@@ -176,14 +176,18 @@ class ColorizedFormatter(Formatter):
         else:
             return Formatter.format(self, record, *args, **kwargs)
 
-_logDir = conf.supybot.directories.log()
-if not os.path.exists(_logDir):
-    os.mkdir(_logDir, 0o755)
+def _mkDirs():
+    global _logDir, pluginLogDir
+    _logDir = conf.supybot.directories.log()
+    if not os.path.exists(_logDir):
+        os.mkdir(_logDir, 0o755)
 
-pluginLogDir = os.path.join(_logDir, 'plugins')
+    pluginLogDir = os.path.join(_logDir, 'plugins')
 
-if not os.path.exists(pluginLogDir):
-    os.mkdir(pluginLogDir, 0o755)
+    if not os.path.exists(pluginLogDir):
+        os.mkdir(pluginLogDir, 0o755)
+
+_mkDirs()
 
 try:
     messagesLogFilename = os.path.join(_logDir, 'messages.log')