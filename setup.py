8859e1ce51b2b97a7762d49b93e3c1484f81cc7f
Valentin Lorentz <progval+git@progval.net>
2023-05-21 18:50:24 +0200

    setup.py: Add Python 3.11 and 3.12 to classifiers.

--- setup.py
+++ setup.py
@@ -204,6 +204,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Communications :: Chat :: Internet Relay Chat',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
