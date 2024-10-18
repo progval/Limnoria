a40d45cc83d964f23760abe7ff1979a087bba0d8
Valentin Lorentz <progval+git@progval.net>
2024-10-18 22:43:00 +0200

    Add Python 3.13 classifier

--- setup.py
+++ setup.py
@@ -208,6 +208,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Communications :: Chat :: Internet Relay Chat',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
