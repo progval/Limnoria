c59686729ad580403a96c9f8c7c3137fef0687e1
James Lu <james@overdrivenetworks.com>
2025-04-27 17:55:36 -0700

    MessageParser: add link to new usage guide

--- plugins/MessageParser/__init__.py
+++ plugins/MessageParser/__init__.py
@@ -35,8 +35,10 @@ which will trigger the bot to respond if they match anywhere in the message.
 This is useful for those cases when you want a bot response even when the bot
 was not explicitly addressed by name or prefix character.
 
-An updated page of this plugin's documentation is located here:
-https://sourceforge.net/p/gribble/wiki/MessageParser_Plugin/
+You can find usage examples at
+https://docs.limnoria.net/use/messageparser.html
+and
+https://sourceforge.net/p/gribble/wiki/MessageParser_Plugin/ (older reference)
 
 """
 
