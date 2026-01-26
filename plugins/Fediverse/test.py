9b6d1e21bf71f37aea77f9d3daf53c4797b0e1d1
Valentin Lorentz <progval+git@progval.net>
2026-01-26 18:44:10 +0100

    Fediverse: Increase timeout
    This should fix flakiness on our GitHub CI and Debian's

--- plugins/Fediverse/test.py
+++ plugins/Fediverse/test.py
@@ -103,7 +103,7 @@ class NetworkedFediverseTestCase(BaseFediverseTestCase):
 
 
 class NetworklessFediverseTestCase(BaseFediverseTestCase):
-    timeout = 1.0
+    timeout = 3.0
 
     @contextlib.contextmanager
     def mockWebfingerSupport(self, value):
