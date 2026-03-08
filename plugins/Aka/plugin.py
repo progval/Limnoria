7cf95ec5e038fc6bd00e835342fa4443f99ab9f5
Valentin Lorentz <progval+git@progval.net>
2026-03-08 11:19:16 +0100

    Aka: Fix copy-paste

--- plugins/Aka/plugin.py
+++ plugins/Aka/plugin.py
@@ -81,7 +81,8 @@ if sqlite3:
             self.filename = filename.replace('sqlite3', 'sqlalchemy')
 
         def close(self):
-            self.dbs.clear()
+            for engine in self.engines.values()
+                engine.clear()
 
         def get_db(self, channel):
             if channel in self.engines:
