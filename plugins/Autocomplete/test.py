6b15ae7e9510cc43aef7b326d8599b3fa3b7b22a
Valentin Lorentz <progval+git@progval.net>
2026-09-20 21:20:18 +0200

    AutoComplete: Remove obsolete test cases

--- plugins/Autocomplete/test.py
+++ plugins/Autocomplete/test.py
@@ -70,52 +70,41 @@ class AutocompleteTestCase(PluginTestCase):
         )
 
     def testResponse(self):
-        with conf.supybot.protocols.irc.experimentalExtensions.context(True):
-            with conf.supybot.plugins.Autocomplete.enabled.context(True):
-                self._assertAutocompleteResponse("apro", "pos")
+        with conf.supybot.plugins.Autocomplete.enabled.context(True):
+            self._assertAutocompleteResponse("apro", "pos")
 
     def testSingleCommandName(self):
-        with conf.supybot.protocols.irc.experimentalExtensions.context(True):
-            with conf.supybot.plugins.Autocomplete.enabled.context(True):
-                self._assertAutocompleteResponse("apro", "pos")
-                self._assertAutocompleteResponse("apr", "opos")
-                self._assertAutocompleteResponse("tel", "l")
+        with conf.supybot.plugins.Autocomplete.enabled.context(True):
+            self._assertAutocompleteResponse("apro", "pos")
+            self._assertAutocompleteResponse("apr", "opos")
+            self._assertAutocompleteResponse("tel", "l")
 
     def testTwoResults(self):
-        with conf.supybot.protocols.irc.experimentalExtensions.context(True):
-            with conf.supybot.plugins.Autocomplete.enabled.context(True):
-                self._assertAutocompleteResponse("te", "ll\tstplugin")
+        with conf.supybot.plugins.Autocomplete.enabled.context(True):
+            self._assertAutocompleteResponse("te", "ll\tstplugin")
 
     def testCommandNameAndPluginName(self):
-        with conf.supybot.protocols.irc.experimentalExtensions.context(True):
-            with conf.supybot.plugins.Autocomplete.enabled.context(True):
-                self._assertAutocompleteResponse("misc t", "ell")
-                self._assertAutocompleteResponse(
-                    "misc c", "learmores\tompletenick"
-                )
+        with conf.supybot.plugins.Autocomplete.enabled.context(True):
+            self._assertAutocompleteResponse("misc t", "ell")
+            self._assertAutocompleteResponse(
+                "misc c", "learmores\tompletenick"
+            )
 
     def testSinglePluginName(self):
-        with conf.supybot.protocols.irc.experimentalExtensions.context(True):
-            with conf.supybot.plugins.Autocomplete.enabled.context(True):
-                self._assertAutocompleteResponse(
-                    "lat", "er notes\ter remove\ter tell\ter undo"
-                )
+        with conf.supybot.plugins.Autocomplete.enabled.context(True):
+            self._assertAutocompleteResponse(
+                "lat", "er notes\ter remove\ter tell\ter undo"
+            )
 
     def testNextWord(self):
-        with conf.supybot.protocols.irc.experimentalExtensions.context(True):
-            with conf.supybot.plugins.Autocomplete.enabled.context(True):
-                self._assertAutocompleteResponse(
-                    "later", " notes\t remove\t tell\t undo"
-                )
+        with conf.supybot.plugins.Autocomplete.enabled.context(True):
+            self._assertAutocompleteResponse(
+                "later", " notes\t remove\t tell\t undo"
+            )
 
     def testNoResponse(self):
-        with conf.supybot.protocols.irc.experimentalExtensions.context(True):
-            self._sendRequest("apro")
-            self.assertIsNone(self.irc.takeMsg())
-
-        with conf.supybot.plugins.Autocomplete.enabled.context(True):
-            self._sendRequest("apro")
-            self.assertIsNone(self.irc.takeMsg())
+        self._sendRequest("apro")
+        self.assertIsNone(self.irc.takeMsg())
 
 
 class AutocompleteChannelTestCase(ChannelPluginTestCase):
@@ -151,18 +140,12 @@ class AutocompleteChannelTestCase(ChannelPluginTestCase):
         )
 
     def testResponse(self):
-        with conf.supybot.protocols.irc.experimentalExtensions.context(True):
-            with conf.supybot.plugins.Autocomplete.enabled.context(True):
-                self._assertAutocompleteResponse("@apro", "pos")
+        with conf.supybot.plugins.Autocomplete.enabled.context(True):
+            self._assertAutocompleteResponse("@apro", "pos")
 
     def testNoResponse(self):
-        with conf.supybot.protocols.irc.experimentalExtensions.context(True):
-            self._sendRequest("@apro")
-            self.assertIsNone(self.irc.takeMsg())
-
-        with conf.supybot.plugins.Autocomplete.enabled.context(True):
-            self._sendRequest("@apro")
-            self.assertIsNone(self.irc.takeMsg())
+        self._sendRequest("@apro")
+        self.assertIsNone(self.irc.takeMsg())
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
